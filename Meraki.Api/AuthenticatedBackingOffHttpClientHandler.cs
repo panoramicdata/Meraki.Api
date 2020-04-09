6ca270f27a2f34ac3f6f0e56f53447287e149864
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2020-04-09 08:41:21 +0100

    Improved Request/Response logging

--- Meraki.Api/AuthenticatedBackingOffHttpClientHandler.cs
+++ Meraki.Api/AuthenticatedBackingOffHttpClientHandler.cs
@@ -10,6 +10,7 @@ namespace Meraki.Api
 	{
 		private readonly MerakiClientOptions _options;
 		private readonly ILogger _logger;
+		private readonly LogLevel _levelToLogAt = LogLevel.Debug;
 
 		public AuthenticatedBackingOffHttpClientHandler(MerakiClientOptions options, ILogger logger)
 		{
@@ -36,18 +37,36 @@ namespace Meraki.Api
 				// Add the request headers
 				request.Headers.Add("X-Cisco-Meraki-API-Key", _options.ApiKey);
 
+				// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
+				if (_logger.IsEnabled(_levelToLogAt))
+				{
+					_logger.Log(_levelToLogAt, $"Request\r\n{request}");
+					if (request.Content != null)
+					{
+						_logger.Log(_levelToLogAt, "RequestContent\r\n" + await request.Content.ReadAsStringAsync().ConfigureAwait(false));
+					}
+				}
+
 				// Complete the action
 				httpResponseMessage = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
-				var content = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
-				_logger.LogDebug(content);
+				// Only do diagnostic logging if we're at the level we want to enable for as this is more efficient
+				if (_logger.IsEnabled(_levelToLogAt))
+				{
+					_logger.Log(_levelToLogAt, $"Response\r\n{httpResponseMessage}");
+					if (httpResponseMessage.Content != null)
+					{
+						_logger.Log(_levelToLogAt, "ResponseContent\r\n" + await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false));
+					}
+				}
 
+				// As long as we were not given a back-off request then we'll return the response and any further StatusCode handling is up to the caller
 				if ((int)httpResponseMessage.StatusCode != 429)
 				{
 					return httpResponseMessage;
 				}
 
-				// We havea 429.  Back off by increasing amounts with subsequent attempts, with a configurable maximum.
+				// We have a 429.  Back off by increasing amounts with subsequent attempts, with a configurable maximum.
 				// There is no maximum total wait time.
 				await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
 				delay = TimeSpan.FromMilliseconds(Math.Max(delay.TotalMilliseconds * 2, _options.MaxBackOffDelay.TotalMilliseconds));
