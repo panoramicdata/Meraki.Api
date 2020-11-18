69adf437296bae79e60bf0c215108e33cc2c3391
Ryan Halley <ryan.halley@panoramic.systems>
2020-11-18 14:44:45 +0000

    Add GET device management interface settings.

---
+++ Meraki.Api/Data/DeviceManagementInterfaceSettingsDdnsHostNames.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Meraki.Api.Data
+{
+	/// <summary>
+	/// DeviceManagementInterfaceSettingsDdnsHostNames
+	/// </summary>
+	[DataContract]
+	public class DeviceManagementInterfaceSettingsDdnsHostNames
+	{
+		/// <summary>
+		/// The active DDNS hostname
+		/// </summary>
+		[DataMember(Name = "activeDdnsHostname")]
+		public string ActiveDdnsHostname { get; set; }
+
+		/// <summary>
+		/// DDNS hostname Wan1
+		/// </summary>
+		[DataMember(Name = "ddnsHostnameWan1")]
+		public string DdnsHostnameWan1 { get; set; }
+
+		/// <summary>
+		/// DDNS hostname Wan2
+		/// </summary>
+		[DataMember(Name = "ddnsHostnameWan2")]
+		public string DdnsHostnameWan2 { get; set; }
+	}
+}
---
+++ Meraki.Api/Data/DeviceManagementInterfaceSettingsWan.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Meraki.Api.Data
+{
+	/// <summary>
+	/// DeviceManagementInterfaceSettingsWan1
+	/// </summary>
+	[DataContract]
+	public class DeviceManagementInterfaceSettingsWan
+	{
+		/// <summary>
+		/// Is WAN enabled
+		/// </summary>
+		[DataMember(Name = "wanEnabled")]
+		public bool WanEnabled { get; set; }
+
+		/// <summary>
+		/// Is WAN using static IP
+		/// </summary>
+		[DataMember(Name = "usingStaticIp")]
+		public bool UsingStaticIp { get; set; }
+
+		/// <summary>
+		/// The static IP
+		/// </summary>
+		[DataMember(Name = "staticIp")]
+		public string StaticIp { get; set; }
+
+		/// <summary>
+		/// The static subnet mask
+		/// </summary>
+		[DataMember(Name = "staticSubnetMask")]
+		public string StaticSubnetMask { get; set; }
+
+		/// <summary>
+		/// The static gateway IP
+		/// </summary>
+		[DataMember(Name = "staticGatewayIp")]
+		public string StaticGatewayIp { get; set; }
+
+		/// <summary>
+		/// The static DNS
+		/// </summary>
+		[DataMember(Name = "staticDns")]
+		public List<string> StaticDns { get; set; }
+
+		/// <summary>
+		/// The VLAN
+		/// </summary>
+		[DataMember(Name = "vlan")]
+		public string Vlan { get; set; }
+	}
+}
