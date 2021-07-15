f4b16aab94cae8fc3a29d30a5fcaa4a8d13c615a
Lorna Harvey <lorna.harvey@panoramicdata.com>
2021-07-15 17:24:30 +0100

    Deleted endpoint

--- Meraki.Api/Interfaces/IFirewalledServices.cs
+++ Meraki.Api/Interfaces/IFirewalledServices.cs
@@ -22,22 +22,5 @@ namespace Meraki.Api.Interfaces
 		Task<object> GetNetworkFirewalledServices(
 			[AliasAs("networkId")]string networkId
 			);
-
-		/// <summary>
-		/// updateNetworkFirewalledService
-		/// </summary>
-		/// <remarks>
-		/// Updates the accessibility settings for the given service (&#39;ICMP&#39;, &#39;web&#39;, or &#39;SNMP&#39;)
-		/// </remarks>
-		/// <exception cref="Swagger.Client.ApiException">Thrown when fails to make API call</exception>
-		/// <param name="networkId"></param>
-		/// <param name="service"></param>
-		/// <param name="updateNetworkFirewalledService"></param>
-		/// <returns>Task of Object</returns>
-		[Put("/networks/{networkId}/firewalledServices/{service}")]
-		Task<object> UpdateNetworkFirewalledService(
-			[AliasAs("networkId")]string networkId,
-			[AliasAs("service")]string service,
-			[Body]FirewalledServiceUpdateRequest updateNetworkFirewalledService);
 	}
 }
