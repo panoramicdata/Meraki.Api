e2c79d3fc11ad06c22c4063a29ac12574240cb9a
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-09-23 10:57:33 +0100

    Updated ApiAccess for LoginSecurity

--- Meraki.Api/Data/ApiAuthentication.cs
+++ Meraki.Api/Data/ApiAuthentication.cs
@@ -1,10 +1,12 @@
-﻿using System.Runtime.Serialization;
+﻿using Meraki.Api.Attributes;
+using System.Runtime.Serialization;
 
 namespace Meraki.Api.Data
 {
 	[DataContract]
 	public class ApiAuthentication
 	{
+		[ApiAccess(ApiAccess.ReadUpdate)]
 		[DataMember(Name = "ipRestrictionsForKeys")]
 		public IpRestrictionsForKeys IpRestrictionsForKeys { get; set; } = new();
 	}
--- Meraki.Api/Data/IpRestrictionsForKeys.cs
+++ Meraki.Api/Data/IpRestrictionsForKeys.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Meraki.Api.Attributes;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace Meraki.Api.Data
@@ -6,9 +7,11 @@ namespace Meraki.Api.Data
 	[DataContract]
 	public class IpRestrictionsForKeys
 	{
+		[ApiAccess(ApiAccess.ReadUpdate)]
 		[DataMember(Name = "enabled")]
 		public bool Enabled { get; set; }
 
+		[ApiAccess(ApiAccess.ReadUpdate)]
 		[DataMember(Name = "ranges")]
 		public List<string> Ranges { get; set; } = new();
 	}
