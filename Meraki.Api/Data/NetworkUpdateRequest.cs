206fa152645c918db542c730914646c773d9d3bc
David Bond <david@davidbond.net>
2021-09-23 22:48:23 +0100

    Fixed NetworkUpdateRequest.EnrollmentString

--- Meraki.Api/Data/NetworkUpdateRequest.cs
+++ Meraki.Api/Data/NetworkUpdateRequest.cs
@@ -35,7 +35,7 @@ namespace Meraki.Api.Data
 		/// </summary>
 		/// <value>A unique identifier which can be used for device enrollment or easy access through the Meraki SM Registration page or the Self Service Portal. Please note that changing this field may cause existing bookmarks to break.</value>
 		[DataMember(Name = "enrollmentString")]
-		public string EnrollmentString { get; set; } = string.Empty;
+		public string? EnrollmentString { get; set; }
 
 		/// <summary>
 		/// Add any notes or additional information about this network here.
