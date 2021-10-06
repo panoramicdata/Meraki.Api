7a7a2f16d5068411b68879a0f40185b7d5545ab1
Peter Acs <peter.acs@panoramicdata.com>
2021-10-06 11:56:15 +0200

    Code tidy.

--- Meraki.Api/Data/ChangeLogEntry.cs
+++ Meraki.Api/Data/ChangeLogEntry.cs
@@ -13,71 +13,71 @@ namespace Meraki.Api.Data
 		/// <summary>
 		/// The Timestamp
 		/// </summary>
-		[DataMember(Name = "ts")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "ts")]
 		public DateTime TimeStamp { get; set; }
 
 		/// <summary>
 		/// The admin name
 		/// </summary>
-		[DataMember(Name = "adminName")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "adminName")]
 		public string AdminName { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The admin email address
 		/// </summary>
-		[DataMember(Name = "adminEmail")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "adminEmail")]
 		public string AdminEmail { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The admin id
 		/// </summary>
-		[DataMember(Name = "adminId")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "adminId")]
 		public string AdminId { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The page
 		/// </summary>
-		[DataMember(Name = "page")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "page")]
 		public string Page { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The label
 		/// </summary>
-		[DataMember(Name = "label")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "label")]
 		public string Label { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The old value
 		/// </summary>
-		[DataMember(Name = "oldValue")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "oldValue")]
 		public string OldValue { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The new value
 		/// </summary>
-		[DataMember(Name = "newValue")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "newValue")]
 		public string NewValue { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The Name of the Network
 		/// </summary>
-		[DataMember(Name = "networkName")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "networkName")]
 		public string? NetworkName { get; set; }
 
 		/// <summary>
 		/// The Name of the Network
 		/// </summary>
-		[DataMember(Name = "networkId")]
 		[ApiAccess(ApiAccess.Read)]
+		[DataMember(Name = "networkId")]
 		public string? NetworkId { get; set; }
 
 		public override string ToString() => Label;
