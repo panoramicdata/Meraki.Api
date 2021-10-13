af0914d96839ebf54459fa3dde687c06f3c5f737
Peter Acs <peter.acs@panoramicdata.com>
2021-10-13 08:48:04 +0200

    Updated Netflow endpoint.

--- Meraki.Api/Interfaces/INetworks.cs
+++ Meraki.Api/Interfaces/INetworks.cs
@@ -211,5 +211,29 @@ namespace Meraki.Api.Interfaces
 		Task<List<Device>> GetAllDevicesAsync(
 			[AliasAs("networkId")] string networkId,
 			CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Return the NetFlow traffic reporting settings for a network
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		[Get("/networks/{networkId}/netflow")]
+		Task<Netflow> GetNetflowAsync(
+			[AliasAs("networkId")] string networkId,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
+		/// Update the NetFlow traffic reporting settings for a network
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		/// <param name="netflow">Body for updating netflow traffic</param>
+		[Put("/networks/{networkId}/netflow")]
+		Task<Netflow> UpdateNetflowAsync(
+			[AliasAs("networkId")] string networkId,
+			[Body] Netflow netflow,
+			CancellationToken cancellationToken = default
+			);
 	}
 }
