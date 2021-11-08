45abf79d9dbb85a32b34161c9e22eae10e83dd83
Peter Acs <peter.acs@panoramicdata.com>
2021-11-08 18:57:29 +0100

    Fixed wrong camera settings endpoint.

--- Meraki.Api/Interfaces/ICameras.cs
+++ Meraki.Api/Interfaces/ICameras.cs
@@ -78,7 +78,7 @@ namespace Meraki.Api.Interfaces
 		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 		/// <param name="serial">The serial id</param>
 		[ApiOperationId("getDeviceCameraVideoSettings")]
-		[Get("/devices/{serial}/camera/settings")]
+		[Get("/devices/{serial}/camera/video/settings")]
 		Task<CameraVideo> GetVideoAsync(
 			[AliasAs("serial")] string serial,
 			CancellationToken cancellationToken = default);
@@ -90,7 +90,7 @@ namespace Meraki.Api.Interfaces
 		/// <param name="serial">The serial id</param>
 		/// <param name="cameraVideoSetting">Body for updating device camera video setting</param>
 		[ApiOperationId("updateDeviceCameraVideoSettings")]
-		[Put("/devices/{serial}/camera/settings")]
+		[Put("/devices/{serial}/camera/video/settings")]
 		Task<CameraVideo> UpdateVideoAsync(
 			[AliasAs("serial")] string serial,
 			[Body] CameraVideo cameraVideoSetting,
