7f58f3dd3113f2a09eddfd5babe4967b48f6ba8d
Peter Acs <peter.acs@panoramicdata.com>
2021-11-09 22:24:10 +0100

    Updated PortSchedule model.

--- Meraki.Api/Data/PortSchedule.cs
+++ Meraki.Api/Data/PortSchedule.cs
@@ -1,3 +1,4 @@
+using Meraki.Api.Attributes;
 using System.Runtime.Serialization;
 
 namespace Meraki.Api.Data
@@ -11,42 +12,49 @@ namespace Meraki.Api.Data
 		/// <summary>
 		/// Gets or Sets Monday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "monday")]
 		public DaySchedule? Monday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Tuesday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "tuesday")]
 		public DaySchedule? Tuesday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Wednesday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "wednesday")]
 		public DaySchedule? Wednesday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Thursday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "thursday")]
 		public DaySchedule? Thursday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Friday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "friday")]
 		public DaySchedule? Friday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Saturday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "saturday")]
 		public DaySchedule? Saturday { get; set; }
 
 		/// <summary>
 		/// Gets or Sets Sunday
 		/// </summary>
+		[ApiAccess(ApiAccess.ReadWrite)]
 		[DataMember(Name = "sunday")]
 		public DaySchedule? Sunday { get; set; }
 	}
