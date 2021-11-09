b8b91315d630cfc0bb09acf2c341dac0d12fbf64
Peter Acs <peter.acs@panoramicdata.com>
2021-11-09 17:12:30 +0100

    Updated Network Switch Routing Multicast Rendezvous Points endpoints.

--- Meraki.Api/Interfaces/ISwitchSettings.cs
+++ Meraki.Api/Interfaces/ISwitchSettings.cs
@@ -109,6 +109,74 @@ namespace Meraki.Api.Interfaces
 			);
 
 		/// <summary>
+		/// List multicast rendezvous points
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		[ApiOperationId("getNetworkSwitchRoutingMulticastRendezvousPoints")]
+		[Get("/networks/{networkId}/switch/routing/multicast/rendezvousPoints")]
+		Task<List<RendezvousPoint>> GetNetworkSwitchRoutingMulticastRendezvousPoints(
+			[AliasAs("networkId")] string networkId,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
+		/// Create a multicast rendezvous points
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		/// <param name="rendezvousPoint">The rendezvous point to create</param>
+		/// <param name="cancellationToken"></param>
+		[ApiOperationId("createNetworkSwitchRoutingMulticastRendezvousPoint")]
+		[Post("/networks/{networkId}/switch/routing/multicast/rendezvousPoints")]
+		Task<RendezvousPoint> CreateNetworkSwitchRoutingMulticastRendezvousPoint(
+			[AliasAs("networkId")] string networkId,
+			[Body] RendezvousPointCreateUpdateRequest rendezvousPoint,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
+		/// Get a multicast rendezvous points
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		[ApiOperationId("getNetworkSwitchRoutingMulticastRendezvousPoint")]
+		[Get("/networks/{networkId}/switch/routing/multicast/rendezvousPoints/{rendezvousPointId}")]
+		Task<RendezvousPoint> GetNetworkSwitchRoutingMulticastRendezvousPoint(
+			[AliasAs("networkId")] string networkId,
+			[AliasAs("rendezvousPointId")] string rendezvousPointId,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
+		/// Update a multicast rendezvous points
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		/// <param name="rendezvousPoint">The rendezvous point to update</param>
+		/// <param name="cancellationToken"></param>
+		[ApiOperationId("updateNetworkSwitchRoutingMulticastRendezvousPoint")]
+		[Put("/networks/{networkId}/switch/routing/multicast/rendezvousPoints/{rendezvousPointId}")]
+		Task<RendezvousPoint> UpdateNetworkSwitchRoutingMulticastRendezvousPoint(
+			[AliasAs("networkId")] string networkId,
+			[Body] RendezvousPointCreateUpdateRequest rendezvousPoint,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
+		/// Delete a multicast rendezvous points
+		/// </summary>
+		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+		/// <param name="networkId">The network id</param>
+		[ApiOperationId("deleteNetworkSwitchRoutingMulticastRendezvousPoint")]
+		[Delete("/networks/{networkId}/switch/routing/multicast/rendezvousPoints/{rendezvousPointId}")]
+		Task<RendezvousPoint> DeleteNetworkSwitchRoutingMulticastRendezvousPoint(
+			[AliasAs("networkId")] string networkId,
+			[AliasAs("rendezvousPointId")] string rendezvousPointId,
+			CancellationToken cancellationToken = default
+			);
+
+		/// <summary>
 		/// Return a quality of service rule
 		/// </summary>
 		/// <exception cref="ApiException">Thrown when fails to make API call</exception>
