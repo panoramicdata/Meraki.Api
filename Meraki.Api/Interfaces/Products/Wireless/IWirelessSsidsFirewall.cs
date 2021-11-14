1eb8f7b2fc55c4dbbecedf90855c8431a950b7f4
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-14 12:05:42 +0000

    Moved wirless L3 firewall rules

--- Meraki.Api/Interfaces/Products/Wireless/IWirelessSsidsFirewall.cs
+++ Meraki.Api/Interfaces/Products/Wireless/IWirelessSsidsFirewall.cs
@@ -29,4 +29,32 @@ public interface IWirelessSsidsFirewall
 	[Body] SsidL7FirewallRules updateNetworkWirelessSsidFirewallL7FirewallRules,
 	CancellationToken cancellationToken = default
 		);
+
+	/// <summary>
+	/// Return the L3 firewall rules for an SSID on an MR network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="number">The SSID number</param>
+	[Get("/networks/{networkId}/ssids/{number}/l3FirewallRules")]
+	Task<SsidLayer3FirewallRulesUpdateRequest> GetNetworkSsidL3FirewallRulesAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("number")] string number,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Update the L3 firewall rules of an SSID on an MR network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="number">The SSID number</param>
+	/// <param name="updateNetworkSsidL3FirewallRules">Body for updating L3 firewall rules</param>
+	[Put("/networks/{networkId}/ssids/{number}/l3FirewallRules")]
+	Task<SsidLayer3FirewallRulesUpdateRequest> UpdateNetworkSsidL3FirewallRulesAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("number")] string number,
+		[Body] SsidLayer3FirewallRulesUpdateRequest updateNetworkSsidL3FirewallRules,
+		CancellationToken cancellationToken = default
+		);
 }
