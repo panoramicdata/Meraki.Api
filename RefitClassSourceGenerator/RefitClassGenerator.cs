d53f96196e84d5418eb57d6f4d81a5a7fff37efb
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-17 10:26:21 +0000

    Fixed missing System.Diagnostics

--- RefitClassSourceGenerator/RefitClassGenerator.cs
+++ RefitClassSourceGenerator/RefitClassGenerator.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
