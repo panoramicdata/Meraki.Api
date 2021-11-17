09a9bce2f5f49b7f610c0fe11e0e0dbab2c8e3e9
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-17 10:44:49 +0000

    Added comment for clarity and ordered endpoints

--- Meraki.ApiChecker/InterfaceReader.cs
+++ Meraki.ApiChecker/InterfaceReader.cs
@@ -19,9 +19,9 @@ public static class InterfaceReader
 
 		var implementedEndpoints = new Dictionary<string, List<MethodDetails>>();
 
-		foreach (var iface in interfaces)
+		foreach (var iface in interfaces.OrderBy(i => i.FullName))
 		{
-			// Look for all the methods and check for ones with Refit HttpMethodAttributes
+			// Look for all the methods and check for ones with Refit HttpMethodAttributes - this will only get public methods, not internal ones
 			foreach (var method in iface.GetMethods())
 			{
 				// There shouldn't be more than one HttpMethodAttribute assigned
