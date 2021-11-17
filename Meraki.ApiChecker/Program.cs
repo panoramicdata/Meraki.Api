53e7b359992fc042d5ecf43cf3f2bced985c3d22
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-17 10:46:14 +0000

    Added messasges for processing

--- Meraki.ApiChecker/Program.cs
+++ Meraki.ApiChecker/Program.cs
@@ -2,8 +2,12 @@
 
 Console.WriteLine($"{ThisAssembly.AssemblyName} v{ThisAssembly.AssemblyInformationalVersion}");
 
+Console.Write("Getting implemented endpoints...");
 var implementedEndpoints = InterfaceReader.GetEndPoints();
+Console.WriteLine("done.");
+Console.Write("Retrieving API schema...");
 var apiSchema = await OpenApiReader.GetSchemaAsync().ConfigureAwait(false);
+Console.WriteLine("done.");
 
 TableOutput.DisplayAndPruneTag(apiSchema, implementedEndpoints, "configure");
 TableOutput.DisplayAndPruneTag(apiSchema, implementedEndpoints, "monitor");
