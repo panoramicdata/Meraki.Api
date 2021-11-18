a3ed6a7960fe49b9fcc1185049a7a327198465b2
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-18 09:55:33 +0000

    Fixed ApiUsages model

--- Meraki.Api/Data/ApiUsage.cs
+++ Meraki.Api/Data/ApiUsage.cs
@@ -6,51 +6,57 @@
 [DataContract]
 public class ApiUsage
 {
-	/// <summary>
-	/// Admin Id
-	/// </summary>
-	[DataMember(Name = "adminId")]
-	public string AdminId { get; set; } = null!;
+    /// <summary>
+    /// Admin Id
+    /// </summary>
+    [DataMember(Name = "adminId")]
+    public string AdminId { get; set; } = null!;
 
-	/// <summary>
-	/// method
-	/// </summary>
-	[DataMember(Name = "method")]
-	public string Method { get; set; } = null!;
+    /// <summary>
+    /// method
+    /// </summary>
+    [DataMember(Name = "method")]
+    public string Method { get; set; } = null!;
 
-	/// <summary>
-	/// host
-	/// </summary>
-	[DataMember(Name = "host")]
-	public string Host { get; set; } = null!;
+    /// <summary>
+    /// host
+    /// </summary>
+    [DataMember(Name = "host")]
+    public string Host { get; set; } = null!;
 
-	/// <summary>
-	/// path
-	/// </summary>
-	[DataMember(Name = "path")]
-	public string Path { get; set; } = null!;
+    /// <summary>
+    /// path
+    /// </summary>
+    [DataMember(Name = "path")]
+    public string Path { get; set; } = null!;
 
-	/// <summary>
-	/// queryString
-	/// </summary>
-	[DataMember(Name = "queryString")]
-	public string QueryString { get; set; } = null!;
+    /// <summary>
+    /// queryString
+    /// </summary>
+    [DataMember(Name = "queryString")]
+    public string QueryString { get; set; } = null!;
 
-	/// <summary>
-	/// userAgent
-	/// </summary>
-	[DataMember(Name = "userAgent")]
-	public string UserAgent { get; set; } = null!;
+    /// <summary>
+    /// User Agent
+    /// </summary>
+    [DataMember(Name = "userAgent")]
+    public string UserAgent { get; set; } = null!;
 
-	/// <summary>
-	/// ts
-	/// </summary>
-	[DataMember(Name = "ts")]
-	public DateTime Timestamp { get; set; }
+    /// <summary>
+    /// Timestamp
+    /// </summary>
+    [DataMember(Name = "ts")]
+    public DateTime Timestamp { get; set; }
 
-	/// <summary>
-	/// Response code
-	/// </summary>
-	[DataMember(Name = "responseCode")]
-	public int ResponseCode { get; set; }
+    /// <summary>
+    /// Response code
+    /// </summary>
+    [DataMember(Name = "responseCode")]
+    public int ResponseCode { get; set; }
+
+    /// <summary>
+    /// Source IP
+    /// </summary>
+    [DataMember(Name = "sourceIp")]
+    public string SourceIp { get; set; }
 }
