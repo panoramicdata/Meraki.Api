df20176c9fe9089287dfd80724a1af251edfe308
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-11-19 12:05:55 +0000

    Removed empty test file and todo comments

--- RefitClassSourceGeneratorApp/Program.cs
+++ RefitClassSourceGeneratorApp/Program.cs
@@ -8,11 +8,11 @@ namespace TestConsoleApp;
 
 class Program
 {
-    static void Main()
-    {
-        string source =
+	static void Main()
+	{
+		string source =
 
-            @"
+			@"
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -50,52 +50,52 @@ namespace Meraki.Api.Sections.General.Networks {
 }
 ";
 
-        var (diagnostics, output) = GetGeneratedOutput(source);
+		var (diagnostics, output) = GetGeneratedOutput(source);
 
-        if (diagnostics.Length > 0)
-        {
-            Console.WriteLine("Diagnostics:");
-            foreach (var diag in diagnostics)
-            {
-                Console.WriteLine("   " + diag.ToString());
-            }
-            Console.WriteLine();
-            Console.WriteLine("Output:");
-        }
+		if (diagnostics.Length > 0)
+		{
+			Console.WriteLine("Diagnostics:");
+			foreach (var diag in diagnostics)
+			{
+				Console.WriteLine("   " + diag.ToString());
+			}
+			Console.WriteLine();
+			Console.WriteLine("Output:");
+		}
 
-        Console.WriteLine(output);
-    }
+		Console.WriteLine(output);
+	}
 
-    private static (ImmutableArray<Diagnostic>, string) GetGeneratedOutput(string source)
-    {
-        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+	private static (ImmutableArray<Diagnostic>, string) GetGeneratedOutput(string source)
+	{
+		var syntaxTree = CSharpSyntaxTree.ParseText(source);
 
-        var references = new List<MetadataReference>();
-        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        foreach (var assembly in assemblies)
-        {
-            if (!assembly.IsDynamic)
-            {
-                references.Add(MetadataReference.CreateFromFile(assembly.Location));
-            }
-        }
+		var references = new List<MetadataReference>();
+		Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+		foreach (var assembly in assemblies)
+		{
+			if (!assembly.IsDynamic)
+			{
+				references.Add(MetadataReference.CreateFromFile(assembly.Location));
+			}
+		}
 
-        var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+		var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
-        // TODO: Uncomment these lines if you want to return immediately if the injected program isn't valid _before_ running generators
-        //
-        // ImmutableArray<Diagnostic> compilationDiagnostics = compilation.GetDiagnostics();
-        //
-        // if (diagnostics.Any())
-        // {
-        //     return (diagnostics, "");
-        // }
+		// Uncomment these lines if you want to return immediately if the injected program isn't valid _before_ running generators
+		//
+		// ImmutableArray<Diagnostic> compilationDiagnostics = compilation.GetDiagnostics();
+		//
+		// if (diagnostics.Any())
+		// {
+		//     return (diagnostics, "");
+		// }
 
-        ISourceGenerator generator = new RefitClassGenerator();
+		ISourceGenerator generator = new RefitClassGenerator();
 
-        var driver = CSharpGeneratorDriver.Create(generator);
-        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+		var driver = CSharpGeneratorDriver.Create(generator);
+		driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
 
-        return (generateDiagnostics, outputCompilation.SyntaxTrees.Last().ToString());
-    }
+		return (generateDiagnostics, outputCompilation.SyntaxTrees.Last().ToString());
+	}
 }
