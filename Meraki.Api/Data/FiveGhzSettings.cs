a79136cb1bc9cc76c18e115f137e38d896ed5c35
Peter Acs <peter.acs@panoramicdata.com>
2021-11-24 12:18:31 +0100

    Code tidy.

--- Meraki.Api/Data/FiveGhzSettings.cs
+++ Meraki.Api/Data/FiveGhzSettings.cs
@@ -9,42 +9,36 @@ public class FiveGhzSettings
 	/// <summary>
 	/// Sets max power (dBm) of 5Ghz band. Can be integer between 8 and 30. Defaults to 30.
 	/// </summary>
-	/// <value>Sets max power (dBm) of 5Ghz band. Can be integer between 8 and 30. Defaults to 30.</value>
 	[DataMember(Name = "maxPower")]
 	public int? MaxPower { get; set; }
 
 	/// <summary>
 	/// Sets min power (dBm) of 5Ghz band. Can be integer between 8 and 30. Defaults to 8.
 	/// </summary>
-	/// <value>Sets min power (dBm) of 5Ghz band. Can be integer between 8 and 30. Defaults to 8.</value>
 	[DataMember(Name = "minPower")]
 	public int? MinPower { get; set; }
 
 	/// <summary>
 	/// Sets min bitrate (Mbps) of 5Ghz band. Can be one of '6', '9', '12', '18', '24', '36', '48' or '54'. Defaults to 12.
 	/// </summary>
-	/// <value>Sets min bitrate (Mbps) of 5Ghz band. Can be one of '6', '9', '12', '18', '24', '36', '48' or '54'. Defaults to 12.</value>
 	[DataMember(Name = "minBitrate")]
 	public int? MinBitrate { get; set; }
 
 	/// <summary>
 	/// Sets valid auto channels for 5Ghz band. Can be one of '36', '40', '44', '48', '52', '56', '60', '64', '100', '104', '108', '112', '116', '120', '124', '128', '132', '136', '140', '144', '149', '153', '157', '161' or '165'.Defaults to [36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165].
 	/// </summary>
-	/// <value>Sets valid auto channels for 5Ghz band. Can be one of '36', '40', '44', '48', '52', '56', '60', '64', '100', '104', '108', '112', '116', '120', '124', '128', '132', '136', '140', '144', '149', '153', '157', '161' or '165'.Defaults to [36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165].</value>
 	[DataMember(Name = "validAutoChannels")]
 	public List<int?> ValidAutoChannels { get; set; } = new();
 
 	/// <summary>
 	/// Sets channel width (MHz) for 5Ghz band. Can be one of 'auto', '20', '40' or '80'. Defaults to auto.
 	/// </summary>
-	/// <value>Sets channel width (MHz) for 5Ghz band. Can be one of 'auto', '20', '40' or '80'. Defaults to auto.</value>
 	[DataMember(Name = "channelWidth")]
 	public string ChannelWidth { get; set; } = string.Empty;
 
 	/// <summary>
 	/// The RX-SOP level controls the sensitivity of the radio. It is strongly recommended to use RX-SOP only after     consulting a wireless expert. RX-SOP can be configured in the range of -65 to -95 (dBm). A value of null will     reset this to the default.
 	/// </summary>
-	/// <value>The RX-SOP level controls the sensitivity of the radio. It is strongly recommended to use RX-SOP only after     consulting a wireless expert. RX-SOP can be configured in the range of -65 to -95 (dBm). A value of null will     reset this to the default.</value>
 	[DataMember(Name = "rxsop")]
 	public int? Rxsop { get; set; }
 }
