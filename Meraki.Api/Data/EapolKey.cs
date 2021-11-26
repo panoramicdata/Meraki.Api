7033a19fd866baf1a5d573724fa770ca08047b95
Peter Acs <peter.acs@panoramicdata.com>
2021-11-26 15:45:23 +0100

    Updated Network Wireless Ssid Eap Override.

---
+++ Meraki.Api/Data/EapolKey.cs
@@ -0,0 +1,20 @@
+﻿namespace Meraki.Api.Data;
+
+[DataContract]
+public class EapolKey
+{
+	/// <summary>
+	/// Maximum number of EAPOL key retries.
+	/// </summary>
+	[ApiAccess(ApiAccess.Update)]
+	[DataMember(Name = "retries")]
+	public int Retries { get; set; }
+
+
+	/// <summary>
+	/// EAPOL Key timeout in milliseconds.
+	/// </summary>
+	[ApiAccess(ApiAccess.Update)]
+	[DataMember(Name = "timeoutInMs")]
+	public int TimeoutInMs { get; set; }
+}
