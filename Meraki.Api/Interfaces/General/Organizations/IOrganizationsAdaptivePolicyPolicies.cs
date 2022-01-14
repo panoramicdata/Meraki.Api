3cdb17ec29be52f156d8842a5d83b1637fef8880
Lorna Harvey <lorna.harvey@panoramicdata.com>
2022-01-14 16:02:46 +0000

    Added GetOrganizationAdaptivePolicyPolicies endpoint

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsAdaptivePolicyPolicies.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsAdaptivePolicyPolicies.cs
@@ -2,13 +2,13 @@
 
 public interface IOrganizationsAdaptivePolicyPolicies
 {
-	///// <summary>
-	///// List adaptive policies in an organization
-	///// </summary>
-	///// <exception cref="ApiException">Thrown when fails to make API call</exception>
-	///// <param name="organizationId">The organization id</param>
-	//[Get("/organizations/{organizationId}/adaptivePolicy/policies")]
-	//Task<List<AdaptivePolicyPolicies>> GetOrganizationAdaptivePolicyPoliciesAsync(
-	//	[AliasAs("organizationId")] string organizationId,
-	//	CancellationToken cancellationToken = default);
+	/// <summary>
+	/// List adaptive policies in an organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">The organization id</param>
+	[Get("/organizations/{organizationId}/adaptivePolicy/policies")]
+	Task<List<AdaptivePolicyPolicies>> GetOrganizationAdaptivePolicyPoliciesAsync(
+		[AliasAs("organizationId")] string organizationId,
+		CancellationToken cancellationToken = default);
 }
