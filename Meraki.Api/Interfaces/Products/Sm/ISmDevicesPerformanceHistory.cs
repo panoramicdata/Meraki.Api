09a318ee8d68ef17c80d763a9c51d7db6bbf7a0d
Lorna Harvey <lorna.harvey@panoramicdata.com>
2022-01-15 13:30:49 +0000

    Updated Sm Device Performance History endpoint

--- Meraki.Api/Interfaces/Products/Sm/ISmDevicesPerformanceHistory.cs
+++ Meraki.Api/Interfaces/Products/Sm/ISmDevicesPerformanceHistory.cs
@@ -7,14 +7,15 @@ public interface ISmDevicesPerformanceHistory
 	/// </summary>
 	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 	/// <param name="networkId">The network id</param>
-	/// <param name="id">The SM id</param>
+	/// <param name="deviceId">The SM id</param>
 	/// <param name="perPage">The number of entries per page returned. Acceptable range is 3 - 1000. Default is 1000. (optional)</param>
 	/// <param name="startingAfter">A token used by the server to indicate the start of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it. (optional)</param>
 	/// <param name="endingBefore">A token used by the server to indicate the end of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it. (optional)</param>
-	[Get("/networks/{networkId}/sm/{id}/performanceHistory")]
-	Task<List<SmDevicePerformanceHistory>> GetNetworkSmPerformanceHistoryAsync(
+	[ApiOperationId("getNetworkSmDevicePerformanceHistory")]
+	[Get("/networks/{networkId}/sm/devices/{deviceId}/performanceHistory")]
+	Task<List<SmDevicePerformanceHistory>> GetNetworkSmDevicePerformanceHistoryAsync(
 		[AliasAs("networkId")] string networkId,
-		[AliasAs("id")] string id,
+		[AliasAs("deviceId")] string deviceId,
 		[AliasAs("perPage")] int? perPage = 1000,
 		[AliasAs("startingAfter")] string startingAfter = null!,
 		[AliasAs("endingBefore")] string endingBefore = null!,
