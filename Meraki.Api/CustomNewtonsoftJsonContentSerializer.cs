b61bb07dbfe26e776a8d17d1ae5b3e6682d56c48
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-01-24 22:10:49 +0000

    Fixes for stream reset

--- Meraki.Api/CustomNewtonsoftJsonContentSerializer.cs
+++ Meraki.Api/CustomNewtonsoftJsonContentSerializer.cs
@@ -9,6 +9,8 @@ public class CustomNewtonsoftJsonContentSerializer : IHttpContentSerializer
 {
 	private readonly MerakiClientOptions _options;
 	private readonly ILogger _logger;
+	private readonly JsonSerializerSettings _jsonSerializerSettingsWithIgnore;
+	private readonly JsonSerializerSettings _jsonSerializerSettingsWithError;
 	private readonly NewtonsoftJsonContentSerializer _serializerIgnore;
 	private readonly NewtonsoftJsonContentSerializer _serializerError;
 
@@ -16,27 +18,28 @@ public class CustomNewtonsoftJsonContentSerializer : IHttpContentSerializer
 	{
 		_options = options;
 		_logger = logger;
-		_serializerIgnore = new NewtonsoftJsonContentSerializer(
-			new JsonSerializerSettings
-			{
-				// By default nulls should not be rendered out, this will allow the receiving API to apply any defaults.
-				// Use [JsonProperty(NullValueHandling = NullValueHandling.Include)] to send
-				// nulls for specific properties, e.g. disassociating port schedule ids from a port
-				NullValueHandling = NullValueHandling.Ignore,
-				MissingMemberHandling = MissingMemberHandling.Ignore,
-				Converters = new List<JsonConverter> { new StringEnumConverter() }
-			});
+		_jsonSerializerSettingsWithIgnore = new JsonSerializerSettings
+		{
+			// By default nulls should not be rendered out, this will allow the receiving API to apply any defaults.
+			// Use [JsonProperty(NullValueHandling = NullValueHandling.Include)] to send
+			// nulls for specific properties, e.g. disassociating port schedule ids from a port
+			NullValueHandling = NullValueHandling.Ignore,
+			MissingMemberHandling = MissingMemberHandling.Ignore,
+			Converters = new List<JsonConverter> { new StringEnumConverter() }
+		};
+		_jsonSerializerSettingsWithError = new JsonSerializerSettings
+		{
+			// By default nulls should not be rendered out, this will allow the receiving API to apply any defaults.
+			// Use [JsonProperty(NullValueHandling = NullValueHandling.Include)] to send
+			// nulls for specific properties, e.g. disassociating port schedule ids from a port
+			NullValueHandling = NullValueHandling.Ignore,
+			MissingMemberHandling = MissingMemberHandling.Error,
+			Converters = new List<JsonConverter> { new StringEnumConverter() }
+		};
 
-		_serializerError = new NewtonsoftJsonContentSerializer(
-			new JsonSerializerSettings
-			{
-				// By default nulls should not be rendered out, this will allow the receiving API to apply any defaults.
-				// Use [JsonProperty(NullValueHandling = NullValueHandling.Include)] to send
-				// nulls for specific properties, e.g. disassociating port schedule ids from a port
-				NullValueHandling = NullValueHandling.Ignore,
-				MissingMemberHandling = MissingMemberHandling.Error,
-				Converters = new List<JsonConverter> { new StringEnumConverter() }
-			});
+		_serializerIgnore = new NewtonsoftJsonContentSerializer(_jsonSerializerSettingsWithIgnore);
+
+		_serializerError = new NewtonsoftJsonContentSerializer(_jsonSerializerSettingsWithError);
 	}
 
 	public async Task<T?> FromHttpContentAsync<T>(HttpContent content, CancellationToken cancellationToken = default)
@@ -50,19 +53,26 @@ public class CustomNewtonsoftJsonContentSerializer : IHttpContentSerializer
 
 	private async Task<T?> LogWarningOnErrorAndContinueFromHttpContentAsync<T>(HttpContent content, CancellationToken cancellationToken)
 	{
+		using var stream = await content.ReadAsStreamAsync().ConfigureAwait(false);
+		using var reader = new StreamReader(stream);
 		try
 		{
-			return await _serializerError.FromHttpContentAsync<T>(content, cancellationToken).ConfigureAwait(false);
+			var serializer = JsonSerializer.Create(_jsonSerializerSettingsWithError);
+			using var jsonTextReader = new JsonTextReader(reader);
+
+			return serializer.Deserialize<T>(jsonTextReader);
 		}
 		catch (JsonSerializationException ex)
 		{
 			_logger.LogWarning(ex, ex.Message);
 
 			// Reset the stream
-			var stream = await content.ReadAsStreamAsync().ConfigureAwait(false);
 			stream.Seek(0, SeekOrigin.Begin);
 
-			return await _serializerIgnore.FromHttpContentAsync<T>(content, cancellationToken).ConfigureAwait(false);
+			var serializer = JsonSerializer.Create(_jsonSerializerSettingsWithIgnore);
+			using var jsonTextReader = new JsonTextReader(reader);
+
+			return serializer.Deserialize<T>(jsonTextReader);
 		}
 	}
 
