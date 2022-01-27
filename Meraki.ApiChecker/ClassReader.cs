79967390543d55bc2e58172ab2a2c65dd38ecf9a
Peter Acs <peter.acs@panoramicdata.com>
2022-01-27 19:53:24 +0100

    Added ClassReader to detect missing ApiAccess attribute.

---
+++ Meraki.ApiChecker/ClassReader.cs
@@ -0,0 +1,44 @@
+﻿using Meraki.Api.Attributes;
+using System.Reflection;
+
+namespace Meraki.ApiChecker;
+public static class ClassReader
+{
+	internal static Dictionary<string, List<string>> GetDataClasses()
+	{
+		// Load our data classes
+		var apiAssembly = Assembly.Load("Meraki.Api");
+		var classes = apiAssembly
+			.GetTypes()
+			.Where(
+				t => t.IsClass
+				&& t.Namespace?.StartsWith("Meraki.Api.Data") == true
+				).ToList();
+
+		var deficientClasses = new Dictionary<string, List<string>>();
+
+		foreach (var dataClass in classes.OrderBy(c => c.FullName))
+		{
+			foreach (var property in dataClass.GetProperties())
+			{
+				var apiAccessAttribute = property.GetCustomAttributes<ApiAccessAttribute>().ToList();
+				if (apiAccessAttribute?.Count > 0)
+				{
+					continue;
+				}
+
+				if (!deficientClasses.TryGetValue(dataClass.FullName!, out var existingList))
+				{
+					// Create a new list for this class
+					deficientClasses[dataClass.FullName!]
+						= existingList
+						= new List<string>();
+				}
+				// Add the entry to the list
+				existingList.Add(property.Name);
+			}
+		}
+
+		return deficientClasses;
+	}
+}
--- Meraki.ApiChecker/Program.cs
+++ Meraki.ApiChecker/Program.cs
@@ -8,6 +8,9 @@ Console.WriteLine("done.");
 Console.Write("Retrieving API schema...");
 var apiSchema = await OpenApiReader.GetSchemaAsync().ConfigureAwait(false);
 Console.WriteLine("done.");
+Console.WriteLine("Getting deficient classes...");
+var deficientClasses = ClassReader.GetDataClasses();
+Console.WriteLine("done.");
 
 TableOutput.DisplayAndPruneTag(apiSchema, implementedEndpoints, "configure");
 TableOutput.DisplayAndPruneTag(apiSchema, implementedEndpoints, "monitor");
