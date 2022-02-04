23774de7c3e7fd70014dcefb5ba099fc1d3ac397
Lorna Harvey <lorna.harvey@panoramicdata.com>
2022-02-04 16:05:58 +0000

    Added missing endpoints

---
+++ Meraki.Api/Data/WebhookPayloadTemplates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meraki.Api.Data;
+
+/// <summary>
+/// Webhook payload templates
+/// </summary>
+[DataContract]
+public class WebhookPayloadTemplates
+{
+	/// <summary>
+	/// Template id
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "payloadTemplateId")]
+	public string PayloadTemplateId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Type
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "type")]
+	public string Type { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Name
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Body
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "body")]
+	public string Body { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Headers
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "headers")]
+	public PayloadTemplateHeaders Headers { get; set; } = new();
+}
---
+++ Meraki.Api/Interfaces/General/Networks/INetworksWebhooksPayloadTemplates.cs
@@ -0,0 +1,73 @@
+﻿namespace Meraki.Api.Interfaces.General.Networks;
+
+public interface INetworksWebhooksPayloadTemplates
+{
+	/// <summary>
+	/// List the webhook payload templates for a network
+	/// </summary>
+	/// <exception cref="ApiExcept	ion">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	[ApiOperationId("getNetworkWebhooksPayloadTemplates")]
+	[Get("/networks/{networkId}/webhooks/payloadTemplates")]
+	Task<List<WebhookPayloadTemplates>> GetNetworkWebhooksPayloadTemplatesAsync(
+		[AliasAs("networkId")] string networkId,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Create a webhook payload template for a network
+	/// </summary>
+	/// <exception cref="ApiExcept	ion">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="createWebhookPayloadTemplate">Body</param>
+	[ApiOperationId("createNetworkWebhooksPayloadTemplate")]
+	[Post("/networks/{networkId}/webhooks/payloadTemplates")]
+	Task<WebhookPayloadTemplates> CreateNetworkWebhooksPayloadTemplateAsync(
+		[AliasAs("networkId")] string networkId,
+		[Body] WebhookPayloadTemplates createWebhookPayloadTemplate,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Get the webhook payload template for a network
+	/// </summary>
+	/// <exception cref="ApiExcept	ion">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="payloadTemplateId">The payload template id</param>
+	[ApiOperationId("getNetworkWebhooksPayloadTemplate")]
+	[Get("/networks/{networkId}/webhooks/payloadTemplates/{payloadTemplateId}")]
+	Task<WebhookPayloadTemplates> GetNetworkWebhooksPayloadTemplateAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("payloadTemplateId")] string payloadTemplateId,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Update a webhook payload template for a network
+	/// </summary>
+	/// <exception cref="ApiExcept	ion">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="payloadTemplateId">The payload template id</param>
+	[ApiOperationId("updateNetworkWebhooksPayloadTemplate")]
+	[Put("/networks/{networkId}/webhooks/payloadTemplates/{payloadTemplateId}")]
+	Task<WebhookPayloadTemplates> UpdateNetworkWebhooksPayloadTemplateAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("payloadTemplateId")] string payloadTemplateId,
+		[Body] WebhookPayloadTemplates createWebhookPayloadTemplate,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Destroy a webhook payload template for a network. Does not work for included templates ('wpt_00001', 'wpt_00002', 'wpt_00003' or 'wpt_00004')
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="payloadTemplateId">The payload template id</param>
+	[ApiOperationId("deleteNetworkWebhooksPayloadTemplate")]
+	[Delete("/networks/{networkId}/webhooks/payloadTemplates/{payloadTemplateId}")]
+	Task DeleteNetworkWebhooksPayloadTemplateAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("payloadTemplateId")] string payloadTemplateId,
+		CancellationToken cancellationToken = default
+		);
+}
