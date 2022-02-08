1352c7150ca7fdb0b53775aac2bf7a984d40f6e9
Peter Acs <peter.acs@panoramicdata.com>
2022-02-08 13:02:36 +0100

    Code tidy.

--- Meraki.Api/Interfaces/General/Networks/INetworksWebhooksPayloadTemplates.cs
+++ Meraki.Api/Interfaces/General/Networks/INetworksWebhooksPayloadTemplates.cs
@@ -43,6 +43,20 @@ public interface INetworksWebhooksPayloadTemplates
 		);
 
 	/// <summary>
+	/// Destroy a webhook payload template for a network. Does not work for included templates ('wpt_00001', 'wpt_00002', 'wpt_00003' or 'wpt_00004')
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="payloadTemplateId">The payload template id</param>
+	[ApiOperationId("deleteNetworkWebhooksPayloadTemplate")]
+	[Delete("/networks/{networkId}/webhooks/payloadTemplates/{payloadTemplateId}")]
+	Task DeleteNetworkWebhooksPayloadTemplateAsync(
+		[AliasAs("networkId")] string networkId,
+		[AliasAs("payloadTemplateId")] string payloadTemplateId,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
 	/// Update a webhook payload template for a network
 	/// </summary>
 	/// <exception cref="ApiExcept	ion">Thrown when fails to make API call</exception>
@@ -54,20 +68,6 @@ public interface INetworksWebhooksPayloadTemplates
 		[AliasAs("networkId")] string networkId,
 		[AliasAs("payloadTemplateId")] string payloadTemplateId,
 		[Body] WebhookPayloadTemplates createWebhookPayloadTemplate,
-		CancellationToken cancellationToken = default
-		);
-
-	/// <summary>
-	/// Destroy a webhook payload template for a network. Does not work for included templates ('wpt_00001', 'wpt_00002', 'wpt_00003' or 'wpt_00004')
-	/// </summary>
-	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
-	/// <param name="networkId">The network id</param>
-	/// <param name="payloadTemplateId">The payload template id</param>
-	[ApiOperationId("deleteNetworkWebhooksPayloadTemplate")]
-	[Delete("/networks/{networkId}/webhooks/payloadTemplates/{payloadTemplateId}")]
-	Task DeleteNetworkWebhooksPayloadTemplateAsync(
-		[AliasAs("networkId")] string networkId,
-		[AliasAs("payloadTemplateId")] string payloadTemplateId,
 		CancellationToken cancellationToken = default
 		);
 }
