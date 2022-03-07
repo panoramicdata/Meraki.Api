ba4aaa68cfdfdfc111fb749fd5964cad3c3593e7
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-03-07 21:14:23 +0000

    EapOverride are all nullable as they don't have values unless EapOverride is enabled

--- Meraki.Api/Data/EapOverride.cs
+++ Meraki.Api/Data/EapOverride.cs
@@ -11,26 +11,26 @@ public class EapOverride
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "maxRetries")]
-	public int MaxRetries { get; set; }
+	public int? MaxRetries { get; set; }
 
 	/// <summary>
 	/// Maximum number of general EAP retries
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "timeout")]
-	public int Timeout { get; set; }
+	public int? Timeout { get; set; }
 
 	/// <summary>
 	/// EAPOL Key settings.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "eapolKey")]
-	public EapolKey EapolKey { get; set; } = new();
+	public EapolKey? EapolKey { get; set; }
 
 	/// <summary>
 	/// EAP settings for identity requests.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "identity")]
-	public EapolIdentity Identity { get; set; } = new();
+	public EapolIdentity? Identity { get; set; }
 }
