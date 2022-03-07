3b56dc9ed1974da5cc09d1ca7f1b45d47be35270
Peter Acs <peter.acs@panoramicdata.com>
2022-03-07 12:33:32 +0100

    Mark ConfigTemplate ProductTypes as a ForeignKey, as it determines the type of a ConfigTemplate when splitting/joining on Networks.

--- Meraki.Api/Data/ConfigurationTemplate.cs
+++ Meraki.Api/Data/ConfigurationTemplate.cs
@@ -10,6 +10,7 @@ public class ConfigurationTemplate : NetworkOrConfigurationTemplate
 	/// Product types
 	/// </summary>
 	[DataMember(Name = "productTypes")]
+	[ApiForeignKey(typeof(ProductType))]
 	[ApiAccess(ApiAccess.Read)]
 	public override List<ProductType> ProductTypes { get; set; } = new();
 }
