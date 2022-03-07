6bdc4e1f062764a3c3a5313c04ce6fd6d4756731
Peter Acs <peter.acs@panoramicdata.com>
2022-03-07 13:43:15 +0100

    Removed unnecessary attributes.

--- Meraki.Api/Data/CombineOrganizationNetworks.cs
+++ Meraki.Api/Data/CombineOrganizationNetworks.cs
@@ -9,21 +9,18 @@ public class CombineOrganizationNetworksRequest
 	/// <summary>
 	/// The name of the combined network
 	/// </summary>
-	[ApiAccess(ApiAccess.Create)]
 	[DataMember(Name = "name")]
 	public string Name { get; set; } = string.Empty;
 
 	/// <summary>
 	/// A list of the network IDs that will be combined. If an ID of a combined network is included in this list, the other networks in the list will be grouped into that network
 	/// </summary>
-	[ApiAccess(ApiAccess.Create)]
 	[DataMember(Name = "networkIds")]
 	public List<string> NetworkIds { get; set; } = new();
 
 	/// <summary>
 	/// A unique identifier which can be used for device enrollment or easy access through the Meraki SM Registration page or the Self Service Portal. Please note that changing this field may cause existing bookmarks to break. All networks that are part of this combined network will have their enrollment string appended by '-network_type'. If left empty, all exisitng enrollment strings will be deleted.
 	/// </summary>
-	[ApiAccess(ApiAccess.Create)]
 	[DataMember(Name = "enrollmentString")]
 	public string EnrollmentString { get; set; } = string.Empty;
 }
