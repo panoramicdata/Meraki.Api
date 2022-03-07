7b445a98c16c0ae44b164bb8a5ff2cf8c65c318e
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-03-07 20:59:55 +0000

    Fixed EapOverride

--- Meraki.Api/Data/EapolKey.cs
+++ Meraki.Api/Data/EapolKey.cs
@@ -6,14 +6,14 @@ public class EapolKey
 	/// <summary>
 	/// Maximum number of EAPOL key retries.
 	/// </summary>
-	[ApiAccess(ApiAccess.Update)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "retries")]
 	public int Retries { get; set; }
 
 	/// <summary>
 	/// EAPOL Key timeout in milliseconds.
 	/// </summary>
-	[ApiAccess(ApiAccess.Update)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "timeoutInMs")]
 	public int TimeoutInMs { get; set; }
 }
