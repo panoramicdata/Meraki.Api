fe827b4c336547cc4421636efe14c7f707cfa969
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-03-08 16:16:34 +0000

    Updated SsidSplashSettings model

--- Meraki.Api/Data/SplashImage.cs
+++ Meraki.Api/Data/SplashImage.cs
@@ -10,11 +10,11 @@ public class SplashImage
 	/// The extension of the image file.
 	/// </summary>
 	[DataMember(Name = "extension")]
-	public string Extension { get; set; } = string.Empty;
+	public string? Extension { get; set; }
 
 	/// <summary>
 	/// The MD5 value of the image file. Setting this to null will remove the image from the splash page.
 	/// </summary>
 	[DataMember(Name = "md5")]
-	public string Md5 { get; set; } = string.Empty;
+	public string? Md5 { get; set; }
 }
