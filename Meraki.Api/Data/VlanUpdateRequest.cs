16c9ff6b86f2fbf233ec33c75ccd2cd9f8e4944d
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-03-09 17:16:29 +0000

    Updated Vlan and VlanUpdateRequest to allow more nullables for when "dhcpHandling": "Do not respond to DHCP requests"

--- Meraki.Api/Data/VlanUpdateRequest.cs
+++ Meraki.Api/Data/VlanUpdateRequest.cs
@@ -52,13 +52,13 @@ public class VlanUpdateRequest
 	/// </summary>
 	/// <value>The IPs of the DHCP servers that DHCP requests should be relayed to</value>
 	[DataMember(Name = "dhcpRelayServerIps")]
-	public List<string> DhcpRelayServerIps { get; set; } = null!;
+	public List<string>? DhcpRelayServerIps { get; set; }
 
 	/// <summary>
 	/// Gets or Sets DhcpLeaseTime
 	/// </summary>
 	[DataMember(Name = "dhcpLeaseTime")]
-	public DhcpLeaseTime DhcpLeaseTime { get; set; }
+	public DhcpLeaseTime? DhcpLeaseTime { get; set; }
 
 	/// <summary>
 	/// Use DHCP boot options specified in other properties
@@ -86,26 +86,26 @@ public class VlanUpdateRequest
 	/// </summary>
 	/// <value>The DHCP fixed IP assignments on the VLAN. This should be an object that contains mappings from MAC addresses to objects that themselves each contain \&quot;ip\&quot; and \&quot;name\&quot; string fields. See the sample request/response for more details.</value>
 	[DataMember(Name = "fixedIpAssignments")]
-	public object FixedIpAssignments { get; set; } = null!;
+	public object? FixedIpAssignments { get; set; }
 
 	/// <summary>
 	/// The DHCP reserved IP ranges on the VLAN
 	/// </summary>
 	/// <value>The DHCP reserved IP ranges on the VLAN</value>
 	[DataMember(Name = "reservedIpRanges")]
-	public List<ReservedIpRange> ReservedIpRanges { get; set; } = null!;
+	public List<ReservedIpRange>? ReservedIpRanges { get; set; }
 
 	/// <summary>
 	/// The DNS nameservers used for DHCP responses, either \&quot;upstream_dns\&quot;, \&quot;google_dns\&quot;, \&quot;opendns\&quot;, or a newline seperated string of IP addresses or domain names
 	/// </summary>
 	/// <value>The DNS nameservers used for DHCP responses, either \&quot;upstream_dns\&quot;, \&quot;google_dns\&quot;, \&quot;opendns\&quot;, or a newline seperated string of IP addresses or domain names</value>
 	[DataMember(Name = "dnsNameservers")]
-	public string DnsNameservers { get; set; } = null!;
+	public string? DnsNameservers { get; set; }
 
 	/// <summary>
 	/// The list of DHCP options that will be included in DHCP responses. Each object in the list should have \&quot;code\&quot;, \&quot;type\&quot;, and \&quot;value\&quot; properties.
 	/// </summary>
 	/// <value>The list of DHCP options that will be included in DHCP responses. Each object in the list should have \&quot;code\&quot;, \&quot;type\&quot;, and \&quot;value\&quot; properties.</value>
 	[DataMember(Name = "dhcpOptions")]
-	public List<DhcpOption> DhcpOptions { get; set; } = null!;
+	public List<DhcpOption>? DhcpOptions { get; set; }
 }
