4791d2691edb7109db6cd134ce96062b51954f69
Peter Acs <peter.acs@panoramicdata.com>
2022-03-15 14:30:47 +0100

    Added missing attributes to GeoLocation properties.

--- Meraki.Api/Data/FloorPlan.cs
+++ Meraki.Api/Data/FloorPlan.cs
@@ -71,35 +71,41 @@ public class FloorPlan
 	public double Height { get; set; }
 
 	/// <summary>
-	/// Center
+	/// The longitude and latitude of the center of your floor plan. If you want to change the geolocation data of your floor plan,
+	/// either the 'center' or two adjacent corners (e.g. 'topLeftCorner' and 'bottomLeftCorner') must be specified. 
+	/// If 'center' is specified, the floor plan is placed over that point with no rotation. 
+	/// If two adjacent corners are specified, the floor plan is rotated to line up with the two specified points. 
+	/// The aspect ratio of the floor plan's image is preserved regardless of which corners/center are specified. 
+	/// (This means if that more than two corners are specified, only two corners may be used to preserve the floor plan's aspect ratio.).
+	/// No two points can have the same latitude, longitude pair.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "center")]
 	public GeoLocation Center { get; set; } = new();
 
 	/// <summary>
-	/// Bottom left corner
+	/// The longitude and latitude of the bottom left corner of your floor plan.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "bottomLeftCorner")]
 	public GeoLocation BottomLeftCorner { get; set; } = new();
 
 	/// <summary>
-	/// Bottom right corner
+	/// The longitude and latitude of the bottom right corner of your floor plan.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "bottomRightCorner")]
 	public GeoLocation BottomRightCorner { get; set; } = new();
 
 	/// <summary>
-	/// Top left corner
+	/// The longitude and latitude of the top left corner of your floor plan.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "topLeftCorner")]
 	public GeoLocation TopLeftCorner { get; set; } = new();
 
 	/// <summary>
-	/// Top right corner
+	/// The longitude and latitude of the top right corner of your floor plan.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "topRightCorner")]
