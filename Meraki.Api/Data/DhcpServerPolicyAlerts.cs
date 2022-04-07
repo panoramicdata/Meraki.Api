152acc654f01fffaf8b2a56e847386b7b65885a0
Peter Acs <peter.acs@panoramicdata.com>
2022-04-07 14:05:24 +0200

    Added Alerts to DhcpServerPolicy.

---
+++ Meraki.Api/Data/DhcpServerPolicyAlerts.cs
@@ -0,0 +1,15 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// additional information/config options for things like alerts/arp inspection
+/// </summary>
+[DataContract]
+public class DhcpServerPolicyAlerts
+{
+	/// <summary>
+	/// Whether Email alerts are enabled for the DhcpServerPolicy
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember]
+	public DhcpServerPolicyAlertEnabledState Email { get; set; } = new();
+}
