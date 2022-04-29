5d76a84526042b9530a1daa48f585fb26bf1cf6b
Mathew Jones <mathew.jones@panoramicdata.com>
2022-04-29 15:07:34 +0100

    #38 Fix Typo

--- Meraki.Api/Interfaces/General/Networks/INetworksWebhooksWebhookTests.cs
+++ Meraki.Api/Interfaces/General/Networks/INetworksWebhooksWebhookTests.cs
@@ -20,7 +20,7 @@ public interface INetworksWebhooksWebhookTests
 	/// </summary>
 	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 	/// <param name="networkId">The network id</param>
-	/// <param name="webhookTest">Body for creating a network HTTP server webhook test</param>
+	/// <param name="webhookTestRequest">Body for creating a network HTTP server webhook test</param>
 	[Post("/networks/{networkId}/webhooks/webhookTests")]
 	Task<WebhookTestResponse> CreateNetworkWebhooksWebhookTestAsync(
 		[AliasAs("networkId")] string networkId,
