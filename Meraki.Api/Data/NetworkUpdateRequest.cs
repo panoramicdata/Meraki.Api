405ed88d01d44a026d2609448ac2643e17c94068
Mathew Jones <mathew.jones@panoramicdata.com>
2022-05-17 12:47:05 +0100

    DNAA-88 Updated network request models to correct which properties can be null.

--- Meraki.Api/Data/NetworkUpdateRequest.cs
+++ Meraki.Api/Data/NetworkUpdateRequest.cs
@@ -11,21 +11,21 @@ public class NetworkUpdateRequest
 	/// </summary>
 	/// <value>The name of the network</value>
 	[DataMember(Name = "name")]
-	public string Name { get; set; } = string.Empty;
+	public string? Name { get; set; }
 
 	/// <summary>
 	/// The timezone of the network. For a list of allowed timezones, please see the 'TZ' column in the table in &lt;a target&#x3D;'_blank' href&#x3D;'https://en.wikipedia.org/wiki/List_of_tz_database_time_zones'&gt;this article.&lt;/a&gt;
 	/// </summary>
 	/// <value>The timezone of the network. For a list of allowed timezones, please see the 'TZ' column in the table in &lt;a target&#x3D;'_blank' href&#x3D;'https://en.wikipedia.org/wiki/List_of_tz_database_time_zones'&gt;this article.&lt;/a&gt;</value>
 	[DataMember(Name = "timeZone")]
-	public string TimeZone { get; set; } = string.Empty;
+	public string? TimeZone { get; set; }
 
 	/// <summary>
 	/// A space-separated list of tags to be applied to the network
 	/// </summary>
 	/// <value>A space-separated list of tags to be applied to the network</value>
 	[DataMember(Name = "tags")]
-	public List<string> Tags { get; set; } = new();
+	public List<string>? Tags { get; set; }
 
 	/// <summary>
 	/// A unique identifier which can be used for device enrollment or easy access through the Meraki SM Registration page or the Self Service Portal. Please note that changing this field may cause existing bookmarks to break.
