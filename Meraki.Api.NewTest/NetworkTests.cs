8ae1637de8deecf2a312646a47ac8ab0acb127e0
Mathew Jones <mathew.jones@panoramicdata.com>
2022-05-17 15:08:27 +0100

    DNAA-88 Updated unit tests to share common code

--- Meraki.Api.NewTest/NetworkTests.cs
+++ Meraki.Api.NewTest/NetworkTests.cs
@@ -1,40 +1,21 @@
-using Divergic.Logging.Xunit;
 using FluentAssertions;
 using Meraki.Api.Data;
 using Meraki.Api.Extensions;
-using Newtonsoft.Json;
 using System.Net;
 using Xunit.Abstractions;
 
 namespace Meraki.Api.NewTest;
 
-public class NetworkTests
+public class NetworkTests : MerakiClientUnitTest
 {
-	private readonly ICacheLogger _logger;
-
-	public NetworkTests(ITestOutputHelper testOutputHelper)
+	public NetworkTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
 	{
-		_logger = testOutputHelper.BuildLogger();
 	}
 
 	[Fact]
 	public async Task GetNetworks_Succeeds()
 	{
-		// Load the config
-		var testConfig = JsonConvert.DeserializeObject<TestConfig>(
-			await File.ReadAllTextAsync("../../../appsettings.json")
-			);
-		if (testConfig is null)
-		{
-			throw new InvalidDataException("API Config Data is empty");
-		}
-		var merakiClientOptions = new MerakiClientOptions
-		{
-			ApiKey = testConfig.ApiKey,
-			ApiNode = testConfig.ApiNode
-		};
-		var merakiClient = new MerakiClient(merakiClientOptions);
-		var networks = await merakiClient.Organizations.Networks.GetOrganizationNetworksAllAsync(testConfig.OrganizationId);
+		var networks = await TestMerakiClient.Organizations.Networks.GetOrganizationNetworksAllAsync(TestOrganizationId);
 		networks.Should().NotBeNullOrEmpty();
 	}
 
@@ -42,64 +23,37 @@ public class NetworkTests
 	public async Task BasicCrud_Succeeds()
 	{
 		// Load the config
-		var testConfig = JsonConvert.DeserializeObject<TestConfig>(
-			await File.ReadAllTextAsync("../../../appsettings.json")
-			);
-		if (testConfig is null)
-		{
-			throw new InvalidDataException("API Config Data is empty");
-		}
-		var merakiClientOptions = new MerakiClientOptions
-		{
-			ApiKey = testConfig.ApiKey,
-			ApiNode = testConfig.ApiNode
-		};
-		var merakiClient = new MerakiClient(merakiClientOptions, _logger);
+
 
 		//Set the test network name
 		string testNetworkName = "Basic CRUD Test Network";
 		string testAlternateNetworkName = "Altered Basic CRUD Test Network";
 
 		// Check the network is not present
-		var networks = await merakiClient.Organizations.Networks.GetOrganizationNetworksAllAsync(testConfig.OrganizationId);
+		var networks = await TestMerakiClient.Organizations.Networks.GetOrganizationNetworksAllAsync(TestOrganizationId);
 		networks.Should().NotBeNull();
 		networks.Should().NotContain(network => network.Name == testNetworkName, "because the test network should not be present to begin the test");
 
 		// Create the new network settings
-		var createNetworkRequest = new NetworkCreationRequest
-		{
-			Name = testNetworkName,
-			Notes = "Test notes for unit test network.",
-			TimeZone = "Europe/London",
-			ProductTypes = new() {
-				ProductType.Appliance,
-				ProductType.Camera,
-				ProductType.CellularGateway,
-				ProductType.Switch,
-				ProductType.Wireless },
-			Tags = new() { "TestTag1", "TestTag2", "TestTag3" }
-		};
+
 
 		//Create a valid network
-		var network = await merakiClient.Organizations.Networks.CreateOrganizationNetworkAsync
-			(
-				testConfig.OrganizationId,
-				createNetworkRequest
-			);
+		var createNetworkRequest = GetValidNetworkCreationRequest();
+		var network = await CreateValidNetworkAsync();
 		network.Should().NotBeNull();
 
 		try
 		{
 			// Check the network can be retrieved and that its values are those set
-			var retrievedNetwork = await merakiClient.Networks.GetNetworkAsync(network.Id);
+			var retrievedNetwork = await TestMerakiClient.Networks.GetNetworkAsync(network.Id);
 			var expectedNetwork = new Network()
 			{
-				OrganizationId = testConfig.OrganizationId,
+				OrganizationId = TestOrganizationId,
 				Name = createNetworkRequest.Name,
 				Notes = createNetworkRequest.Notes,
-				TimeZone = createNetworkRequest.TimeZone,
+				TimeZone = createNetworkRequest.TimeZone!,
 				ProductTypes = createNetworkRequest.ProductTypes,
-				Tags = createNetworkRequest.Tags,
+				Tags = createNetworkRequest.Tags!,
 				Id = network.Id,
 				IsBoundToConfigTemplate = false
 			};
@@ -112,7 +66,7 @@ public class NetworkTests
 				);
 
 			// Change the network Name
-			var networkUpdated = await merakiClient.Networks.UpdateNetworkAsync
+			var networkUpdated = await TestMerakiClient.Networks.UpdateNetworkAsync
 				(
 				network.Id,
 				new NetworkUpdateRequest
@@ -132,7 +86,7 @@ public class NetworkTests
 				);
 
 			// Retrieve the network again and check the Name has changed
-			var reretrievedNetwork = await merakiClient.Networks.GetNetworkAsync(network.Id);
+			var reretrievedNetwork = await TestMerakiClient.Networks.GetNetworkAsync(network.Id);
 			reretrievedNetwork.Should()
 				.NotBeNull()
 				.And
@@ -144,12 +98,12 @@ public class NetworkTests
 		finally
 		{
 			// Delete the network
-			await merakiClient.Networks.DeleteNetworkAsync(network.Id);
+			await TestMerakiClient.Networks.DeleteNetworkAsync(network.Id);
 		}
 
 		// Check the network is not present
 
-		var exception = await Assert.ThrowsAsync<Refit.ApiException>(() => merakiClient.Networks.GetNetworkAsync(network.Id));
+		var exception = await Assert.ThrowsAsync<Refit.ApiException>(() => TestMerakiClient.Networks.GetNetworkAsync(network.Id));
 		exception.StatusCode.Should().Be(HttpStatusCode.NotFound);
 
 	}
