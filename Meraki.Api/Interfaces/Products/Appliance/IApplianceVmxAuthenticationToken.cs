c259d60c43241dd2b54a1696f8cbf43e5514f045
Mathew Jones <mathew.jones@panoramicdata.com>
2022-06-07 14:00:20 +0100

    Added Create Device Appliance Vmx Authentication Token endpoint at /devices/{serial}/appliance/vmx/authenticationToken

---
+++ Meraki.Api/Interfaces/Products/Appliance/IApplianceVmxAuthenticationToken.cs
@@ -0,0 +1,13 @@
+﻿namespace Meraki.Api.Interfaces.Products.Appliance;
+
+public interface IApplianceVmxAuthenticationToken
+{
+	/// <summary>
+	/// Generate a new vMX authentication token
+	/// </summary>
+	/// <param name="serial">The device serial number</param>
+	[Post("/devices/{serial}/appliance/vmx/authenticationToken")]
+	Task<VmxAuthenticationToken> CreateDeviceApplianceVmxAuthenticationTokenAsync(
+		[AliasAs("serial")] string serial,
+		CancellationToken cancellationToken = default);
+}
