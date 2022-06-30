95e9e1bf2d9818d9d35c1c8a49cc381543aaf05a
David Bond <david@davidbond.net>
2022-06-30 17:02:33 +0100

    Code tidy

--- Meraki.ApiChecker/Extensions/StringExtensions.cs
+++ Meraki.ApiChecker/Extensions/StringExtensions.cs
@@ -1,12 +1,14 @@
-﻿namespace Meraki.ApiChecker.Extensions;
+﻿using System.Globalization;
+
+namespace Meraki.ApiChecker.Extensions;
 
 public static class StringExtensions
 {
-    public static string FirstCharToUpper(this string input)
-        => input switch
-        {
-            null => throw new ArgumentNullException(nameof(input)),
-            "" => "",
-            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
-        };
+	public static string FirstCharToUpper(this string input)
+		=> input switch
+		{
+			null => throw new ArgumentNullException(nameof(input)),
+			"" => "",
+			_ => string.Concat(input[0].ToString().ToUpper(CultureInfo.InvariantCulture), input.AsSpan(1))
+		};
 }
