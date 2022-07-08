76627081b6e13b7afb0981673bbfed18c2d56865
Mathew Jones <mathew.jones@panoramicdata.com>
2022-07-08 15:47:15 +0100

    #56  Added Get Network Sm Trusted Access Configs endpoint

---
+++ Meraki.Api/Data/SmTrustedAccessConfig.cs
@@ -0,0 +1,38 @@
+﻿namespace Meraki.Api.Data;
+public class SmTrustedAccessConfig : NamedIdentifiedItem
+{
+	/// <summary>
+	/// SSID Name
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "ssidName")]
+	public string? SsidName { get; set; }
+
+	/// <summary>
+	/// Scope
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "scope")]
+	public string? scope { get; set; }
+
+	/// <summary>
+	/// Tags
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "tags")]
+	public List<string>? Tags { get; set; }
+
+	/// <summary>
+	/// Access Start At
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "accessStartAt")]
+	public DateTime? accessStartAt { get; set; }
+
+	/// <summary>
+	/// Access End At
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "accessEndAt")]
+	public DateTime? accessEndAt { get; set; }
+}
