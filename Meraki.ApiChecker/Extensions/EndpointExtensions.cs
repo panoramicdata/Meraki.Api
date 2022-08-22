4c8a4dccc547af4fd818709496af207447abbb14
stashjones <74157020+stashjones@users.noreply.github.com>
2022-08-22 12:46:30 +0100

    Sheet creation moved to extention method and empty sheets excluded (#62)
    * Sheet creation moved to extention method and empty sheets excluded from result
    * Code tidy

---
+++ Meraki.ApiChecker/Extensions/EndpointExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Meraki.ApiChecker.Data;
+using PanoramicData.SheetMagic;
+
+namespace Meraki.ApiChecker.Extensions;
+internal static class EndpointExtensions
+{
+	internal static void AddToWorkbook(this EndpointSet endpointSet, MagicSpreadsheet workbook, string setName)
+	{
+		if (endpointSet.ImplementedEndpoints.Count > 0)
+		{
+			workbook.AddSheet(endpointSet.ImplementedEndpoints, setName + "-Implemented");
+		}
+
+		if (endpointSet.DuplicatedEndpoints.Count > 0)
+		{
+			workbook.AddSheet(endpointSet.DuplicatedEndpoints, setName + "-Duplicated");
+		}
+
+		if (endpointSet.MissingEndpoints.Count > 0)
+		{
+			workbook.AddSheet(endpointSet.MissingEndpoints, setName + "-Missing");
+		}
+	}
+}
