3c16d73688593d63ff853da00f5f5ae1d3c1984b
Mathew Jones <mathew.jones@panoramicdata.com>
2022-10-31 14:07:53 +0000

    nullability of video settings

--- Meraki.Api/Data/VideoSettings.cs
+++ Meraki.Api/Data/VideoSettings.cs
@@ -11,28 +11,28 @@ public class VideoSettings
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV2")]
-	public Mv02 MV2 { get; set; } = null!;
+	public Mv02? MV2 { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV21MV71
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV21/MV71")]
-	public Mv21Mv71 Mv21Mv71 { get; set; } = null!;
+	public Mv21Mv71? Mv21Mv71 { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV12MV22MV72
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV12/MV22/MV72")]
-	public Mv12Mv22Mv72 Mv12Mv22Mv72 { get; set; } = null!;
+	public Mv12Mv22Mv72? Mv12Mv22Mv72 { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV52
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV52")]
-	public Mv52 Mv52 { get; set; } = null!;
+	public Mv52? Mv52 { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV63
@@ -67,19 +67,19 @@ public class VideoSettings
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV32")]
-	public Mv32 Mv32 { get; set; } = null!;
+	public Mv32? Mv32 { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV12WE
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV12WE")]
-	public Mv12we Mv12we { get; set; } = null!;
+	public Mv12we? Mv12we { get; set; }
 
 	/// <summary>
 	/// Gets or Sets MV22XMV72X
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "MV22X/MV72X")]
-	public Mv22xMv72x Mv22xMv72x { get; set; } = null!;
+	public Mv22xMv72x? Mv22xMv72x { get; set; }
 }
