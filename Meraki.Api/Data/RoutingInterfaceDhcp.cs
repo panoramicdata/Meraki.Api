ac64f63488a7bb9c35d19a0585d05847ae3061d9
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2022-11-30 16:12:49 +0000

    Updated RoutingInterfaceDhcp to allow nulls

--- Meraki.Api/Data/RoutingInterfaceDhcp.cs
+++ Meraki.Api/Data/RoutingInterfaceDhcp.cs
@@ -25,61 +25,61 @@ public class RoutingInterfaceDhcp
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "dhcpLeaseTime")]
-	public string DhcpLeaseTime { get; set; } = string.Empty;
+	public string? DhcpLeaseTime { get; set; }
 
 	/// <summary>
 	/// The DHCP name server option for the dhcp server running on the switch interface ('googlePublicDns', 'openDns' or 'custom')
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "dnsNameserversOption")]
-	public string DnsNameserversOption { get; set; } = string.Empty;
+	public string? DnsNameserversOption { get; set; }
 
 	/// <summary>
 	/// The DHCP name server IPs when DHCP name server option is 'custom'
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "dnsCustomNameservers")]
-	public List<string> DnsCustomNameservers { get; set; } = new();
+	public List<string>? DnsCustomNameservers { get; set; }
 
 	/// <summary>
 	/// Enable DHCP boot options to provide PXE boot options configs for the dhcp server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "bootOptionsEnabled")]
-	public bool BootOptionsEnabled { get; set; }
+	public bool? BootOptionsEnabled { get; set; }
 
 	/// <summary>
 	/// The PXE boot server IP for the DHCP server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "bootNextServer")]
-	public string BootNextServer { get; set; } = string.Empty;
+	public string? BootNextServer { get; set; }
 
 	/// <summary>
 	/// The PXE boot server filename for the DHCP server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "bootFileName")]
-	public string BootFileName { get; set; } = string.Empty;
+	public string? BootFileName { get; set; }
 
 	/// <summary>
 	/// Array of DHCP options consisting of code, type and value for the DHCP server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "dhcpOptions")]
-	public List<DhcpOption> DhcpOptions { get; set; } = new();
+	public List<DhcpOption>? DhcpOptions { get; set; }
 
 	/// <summary>
 	/// Array of DHCP reserved IP assignments for the DHCP server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "ReservedIpRanges")]
-	public List<ReservedIpRange> ReservedIpRanges { get; set; } = new();
+	public List<ReservedIpRange>? ReservedIpRanges { get; set; }
 
 	/// <summary>
 	/// Array of DHCP fixed IP assignments for the DHCP server running on the switch interface
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "fixedIpAssignments")]
-	public List<FixedIpAssignment> FixedIpAssignments { get; set; } = new();
+	public List<FixedIpAssignment>? FixedIpAssignments { get; set; }
 }
