4b466b5baee70b71d537c824a6880b96e7178d9e
Mathew Jones <mathew.jones@panoramicdata.com>
2022-12-12 21:03:32 +0000

    Network Settings Added Client Privacy

--- Meraki.Api/Data/NetworkSettings.cs
+++ Meraki.Api/Data/NetworkSettings.cs
@@ -9,35 +9,42 @@ public class NetworkSettings
 	/// <summary>
 	/// Local status page enabled?
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "localStatusPageEnabled")]
 	public bool? LocalStatusPageEnabled { get; set; }
 
 	/// <summary>
 	/// Remote status page enabled?
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "remoteStatusPageEnabled")]
 	public bool? RemoteStatusPageEnabled { get; set; }
 
 	/// <summary>
 	/// Secure port settings - formerly secure connect
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "securePort")]
 	public NetworkSettingsSecurePort? SecurePort { get; set; }
 
 	/// <summary>
 	/// Fips settings - undocumented
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "fips")]
 	public NetworkSettingsFips? Fips { get; set; }
 
 	/// <summary>
 	/// Local Status PAge
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "localStatusPage")]
 	public LocalStatusPage? LocalStatusPage { get; set; }
+
+	/// <summary>
+	/// Client Privacy
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "clientPrivacy")]
+	public NetworkSettingsClientPrivacy? ClientPrivacy { get; set; }
 }
