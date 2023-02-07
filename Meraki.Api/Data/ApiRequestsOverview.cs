834ce16af4675741e516392fcd5c3fdc5bb823d7
Mathew Jones <mathew.jones@panoramicdata.com>
2023-02-07 12:07:41 +0000

    read only tokens

--- Meraki.Api/Data/ApiRequestsOverview.cs
+++ Meraki.Api/Data/ApiRequestsOverview.cs
@@ -9,18 +9,21 @@ public class ApiRequestsOverview
 	/// <summary>
 	/// startTs
 	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "startTs")]
 	public DateTime StartTs { get; set; }
 
 	/// <summary>
 	/// endTs
 	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "endTs")]
 	public DateTime EndTs { get; set; }
 
 	/// <summary>
 	/// counts
 	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "counts")]
 	public List<ApiRequestsOverviewCount>? Counts { get; set; }
 }
--- Meraki.Api/Data/ApiRequestsOverviewCount.cs
+++ Meraki.Api/Data/ApiRequestsOverviewCount.cs
@@ -9,12 +9,14 @@ public class ApiRequestsOverviewCount
 	/// <summary>
 	/// code
 	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "code")]
 	public int Code { get; set; }
 
 	/// <summary>
 	/// count
 	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "count")]
 	public int Count { get; set; }
 }
