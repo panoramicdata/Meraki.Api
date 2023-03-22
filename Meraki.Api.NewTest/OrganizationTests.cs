c0468a8f56f6e60cad3ce0d30d12fafb0649e667
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2023-03-22 16:03:37 +0000

    Added simple organization test for client compilation proving

---
+++ Meraki.Api.NewTest/OrganizationTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace Meraki.Api.NewTest;
+[Collection("API Collection")]
+public class OrganizationTests : MerakiClientUnitTest
+{
+	public OrganizationTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+	{
+	}
+
+	[Fact]
+	public async Task GetOrganizations_Succeeds()
+	{
+		var organizations = await TestMerakiClient.Organizations.GetOrganizationsAsync(default);
+		_ = organizations.Should().NotBeEmpty();
+	}
+}
