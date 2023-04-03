87b921fbd9a1cba5e3839d5078ee4bb06533d6f3
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2023-04-03 17:09:02 +0100

    Code tidy

--- Meraki.Api/Data/ApplianceSettings.cs
+++ Meraki.Api/Data/ApplianceSettings.cs
@@ -7,14 +7,14 @@
 public class ApplianceSettings
 {
 	/// <summary>
-	/// Client tracking method
+	/// Client tracking method of a network
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "clientTrackingMethod")]
 	public string? ClientTrackingMethod { get; set; }
 
 	/// <summary>
-	/// Deployment Mode
+	/// Deployment mode of a network
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "deploymentMode")]
