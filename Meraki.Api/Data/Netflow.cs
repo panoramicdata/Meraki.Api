945b9b461dac7d05bc9c80b4f008d680f146a34b
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2023-04-17 10:18:12 +0100

    Updated Netflow to allow nullable values

--- Meraki.Api/Data/Netflow.cs
+++ Meraki.Api/Data/Netflow.cs
@@ -11,33 +11,33 @@ public class Netflow
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "reportingEnabled")]
-	public bool ReportingEnabled { get; set; }
+	public bool? ReportingEnabled { get; set; }
 
 	/// <summary>
 	/// The IPv4 address of the NetFlow collector.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "collectorIp")]
-	public string CollectorIp { get; set; } = string.Empty;
+	public string? CollectorIp { get; set; }
 
 	/// <summary>
 	/// The port that the NetFlow collector will be listening on.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "collectorPort")]
-	public int CollectorPort { get; set; }
+	public int? CollectorPort { get; set; }
 
 	/// <summary>
 	/// Boolean indicating whether Encrypted Traffic Analysis is enabled (true) or disabled (false).
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "etaEnabled")]
-	public bool EtaEnabled { get; set; }
+	public bool? EtaEnabled { get; set; }
 
 	/// <summary>
 	/// The port that the Encrypted Traffic Analysis collector will be listening on.
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "etaDstPort")]
-	public int EtaDstPort { get; set; }
+	public int? EtaDstPort { get; set; }
 }
