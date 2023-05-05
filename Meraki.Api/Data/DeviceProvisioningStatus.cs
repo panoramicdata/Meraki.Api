00c680ac2e5b147a29c2063f11072b5b15d07255
Mathew Jones <mathew.jones@panoramicdata.com>
2023-05-05 18:23:31 +0100

    API Changes

---
+++ Meraki.Api/Data/DeviceProvisioningStatus.cs
@@ -0,0 +1,47 @@
+﻿namespace Meraki.Api.Data;
+
+[DataContract]
+public class DeviceProvisioningStatus
+{
+	/// <summary>
+	/// The device MAC address.
+	/// </summary>
+	[DataMember(Name = "mac")]
+	public string? Mac { get; set; }
+
+	/// <summary>
+	/// The device name.
+	/// </summary>
+	[DataMember(Name = "name")]
+	public string? Name { get; set; }
+
+	/// <summary>
+	/// Device product type.
+	/// </summary>
+	[DataMember(Name = "productType")]
+	public string? ProductType { get; set; }
+
+	/// <summary>
+	/// The device serial number.
+	/// </summary>
+	[DataMember(Name = "serial")]
+	public string? Serial { get; set; }
+
+	/// <summary>
+	/// The device provisioning status. Possible statuses: unprovisioned, incomplete, complete.
+	/// </summary>
+	[DataMember(Name = "status")]
+	public DeviceProvisioningStatusStatus? Status { get; set; }
+
+	/// <summary>
+	/// List of custom tags for the device.
+	/// </summary>
+	[DataMember(Name = "tags")]
+	public List<string>? Tags { get; set; }
+
+	/// <summary>
+	/// Network info
+	/// </summary>
+	[DataMember(Name = "network")]
+	public DeviceProvisioningStatusNetwork? Network { get; set; }
+}
---
+++ Meraki.Api/Data/DeviceProvisioningStatusNetwork.cs
@@ -0,0 +1,12 @@
+﻿namespace Meraki.Api.Data;
+
+[DataContract]
+public class DeviceProvisioningStatusNetwork
+{
+	/// <summary>
+	/// ID for the network containing the device
+	/// </summary>
+	[DataMember(Name = "id")]
+	public string? Id { get; set; }
+
+}
