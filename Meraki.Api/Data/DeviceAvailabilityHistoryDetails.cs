bf56a1045b3af93248b57e1ef498515d786a018e
Mathew Jones <mathew.jones@panoramicdata.com>
2023-06-13 10:00:54 +0100

    Get Organization Devices Availabilities History Endpoint Added

---
+++ Meraki.Api/Data/DeviceAvailabilityHistoryDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+/// <summary>
+/// Details about the status changes
+/// </summary>
+[DataContract]
+
+public class DeviceAvailabilityHistoryDetails
+{
+	/// <summary>
+	/// Details about the new status
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "new")]
+	public List<DeviceAvailabilityHistoryDetailNew> New { get; set; } = new();
+
+	/// <summary>
+	/// Details about the old status
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "old")]
+	public List<DeviceAvailabilityHistoryDetailOld> Old { get; set; } = new();
+}
