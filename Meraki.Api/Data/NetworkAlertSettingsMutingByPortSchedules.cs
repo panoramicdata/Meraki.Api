1c64aff857356be54369d54bfde26376687c59ed
Mathew Jones <mathew.jones@panoramicdata.com>
2023-06-26 12:36:53 +0100

    muting added to network alert settings

---
+++ Meraki.Api/Data/NetworkAlertSettingsMutingByPortSchedules.cs
@@ -0,0 +1,15 @@
+namespace Meraki.Api.Data;
+
+/// <summary>
+/// NetworkAlertSettingsMutingByPortSchedules
+/// </summary>
+[DataContract]
+public class NetworkAlertSettingsMutingByPortSchedules
+{
+	/// <summary>
+	/// NetworkAlertSettingsMutingByPortSchedules Enabled - Undocumented as at 2023-06-23
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "enabled")]
+	public bool? Enabled { get; set; }
+}
