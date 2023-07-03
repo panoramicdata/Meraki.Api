bfe0b41576d740c26b33243ab7593cdae16d3da5
David Bond <david.bond@panoramicdata.com>
2023-07-03 19:17:56 +0100

    Added missing sensor reading endpoints

---
+++ Meraki.Api/Sections/Products/Sensor/SensorReadingsSection.cs
@@ -0,0 +1,8 @@
+﻿namespace Meraki.Api.Sections.Products.Sensor;
+
+public class SensorReadingsSection
+{
+	public IOrganizationSensorReadingsLatest Latest { get; internal set; } = null!;
+
+	public IOrganizationSensorReadingsHistory History { get; internal set; } = null!;
+}
