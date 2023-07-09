7266113840761d39dd58e0f5b27b498d0c271861
David Bond <david.bond@panoramicdata.com>
2023-07-09 16:30:06 +0100

    Code tidy

---
+++ Meraki.Api/Data/SensorMetricDownstreamPowerHistoric.cs
@@ -0,0 +1,20 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Sensor metric: downstreamPower for history endpoint
+/// </summary>
+[DataContract]
+public class SensorMetricDownstreamPowerHistoric
+{
+	///<summary>
+	/// Locked
+	/// </summary>
+	[DataMember(Name = "locked")]
+	public bool Locked { get; set; }
+
+	///<summary>
+	/// Enabled
+	/// </summary>
+	[DataMember(Name = "enabled")]
+	public bool Enabled { get; set; }
+}
--- Meraki.Api/Data/SensorMetricDownstreamPowerLatest.cs
+++ Meraki.Api/Data/SensorMetricDownstreamPowerLatest.cs
@@ -18,22 +18,3 @@ public class SensorMetricDownstreamPowerLatest
 	[DataMember(Name = "state")]
 	public SensorMetricDownstreamPowerState State { get; set; }
 }
-
-/// <summary>
-/// Sensor metric: downstreamPower for history endpoint
-/// </summary>
-[DataContract]
-public class SensorMetricDownstreamPowerHistoric
-{
-	///<summary>
-	/// Locked
-	/// </summary>
-	[DataMember(Name = "locked")]
-	public bool Locked { get; set; }
-
-	///<summary>
-	/// Enabled
-	/// </summary>
-	[DataMember(Name = "enabled")]
-	public bool Enabled { get; set; }
-}
