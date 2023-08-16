4c92a41aeae1cbeb60973a08ead77beb304eb8b1
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2023-08-16 09:35:55 +0100

    Fixed issues with EarlyAccessFeature data not being populated into the model

---
+++ Meraki.Api.NewTest/EarlyAccessFeatureTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace Meraki.Api.NewTest;
+public class EarlyAccessFeatureTests : MerakiClientUnitTest
+{
+	public EarlyAccessFeatureTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+	{
+	}
+
+	[Fact]
+	public async Task BasicGet_Features_Suceeds()
+	{
+		var result = await TestMerakiClient.Organizations.EarlyAccess.Features.GetOrganizationEarlyAccessFeaturesAsync(TestOrganizationId);
+		_ = result.Should().NotBeNullOrEmpty();
+
+	}
+}
--- Meraki.Api/Data/EarlyAccessFeature.cs
+++ Meraki.Api/Data/EarlyAccessFeature.cs
@@ -11,33 +11,40 @@ public class EarlyAccessFeature : NamedItem
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "shortName")]
-	public string ShortName { get; } = string.Empty;
+	public string ShortName { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Descriptions
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "descriptions")]
-	public EarlyAccessFeatureDescription Descriptions { get; } = new();
+	public EarlyAccessFeatureDescription Descriptions { get; set; } = new();
 
 	/// <summary>
 	/// Topic
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "topic")]
-	public string Topic { get; } = string.Empty;
+	public string Topic { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Is Organization Scoped Only
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "isOrgScopedOnly")]
-	public bool IsOrgScopedOnly { get; }
+	public bool IsOrgScopedOnly { get; set; }
 
 	/// <summary>
-	/// Documentation Lnk
+	/// Documentation Link
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "documentationLink")]
-	public string? DocumentationLink { get; }
+	public string DocumentationLink { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Support Link
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "supportLink")]
+	public string SupportLink { get; set; } = string.Empty;
 }
