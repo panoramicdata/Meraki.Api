17096bd92bbb3626c3ebc71f452e0762172b0c97
Mathew Jones <mathew.jones@panoramicdata.com>
2023-09-22 08:31:23 +0100

    Corrected naming of WebhookHttpServerPayloadTemplate Id property

--- Meraki.Api/Data/WebHookHttpServerPayloadTemplate.cs
+++ Meraki.Api/Data/WebHookHttpServerPayloadTemplate.cs
@@ -11,7 +11,7 @@ public class WebhookHttpServerPayloadTemplate
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "id")]
-	public string Id { get; set; } = string.Empty;
+	public string payloadTemplateId { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Name
