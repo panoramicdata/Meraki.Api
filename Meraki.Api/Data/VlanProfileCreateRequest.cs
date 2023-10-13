4b39b33ff31d631230b382ca3731d0e13ee5b484
Mathew Jones <mathew.jones@panoramicdata.com>
2023-10-13 12:23:38 +0100

    Vlan Profiles

---
+++ Meraki.Api/Data/VlanProfileCreateRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Vlan Profile
+/// </summary>
+[DataContract]
+public class VlanProfileCreateRequest : NamedItem
+{
+	/// <summary>
+	/// IName of the VLAN profile
+	/// </summary>
+	[DataMember(Name = "iname")]
+	public string? Iname { get; set; }
+
+	/// <summary>
+	/// An array of named VLANs
+	/// </summary>
+	[DataMember(Name = "vlanGroups")]
+	public List<VlanProfileVlanGroup>? VlanGroups { get; set; }
+
+	/// <summary>
+	/// An array of named VLANs
+	/// </summary>
+	[DataMember(Name = "vlanNames")]
+	public List<VlanProfileVlanName>? VlanNames { get; set; }
+}
---
+++ Meraki.Api/Interfaces/General/Networks/INetworksVlanProfiles.cs
@@ -0,0 +1,75 @@
+﻿namespace Meraki.Api.Interfaces.General.Networks;
+
+public interface INetworksVlanProfiles
+
+{
+	/// <summary>
+	/// Return an HTTP server for a network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="vlanProfileCreateRequest">The vlan profile creation request</param>
+	[ApiOperationId("createNetworkVlanProfile")]
+	[Post("/networks/{networkId}/vlanProfiles")]
+	Task<VlanProfile> CreateNetworkVlanProfileAsync(
+		string networkId,
+		[Body] VlanProfileCreateRequest vlanProfileCreateRequest,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// List VLAN profiles for a network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	[ApiOperationId("getNetworkVlanProfiles")]
+	[Get("/networks/{networkId}/vlanProfiles")]
+	Task<List<VlanProfile>> GetNetworkVlanProfilesAsync(
+		string networkId,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Get an existing VLAN profile of a network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="iname">The iname for the Vlan profile</param>
+	[ApiOperationId("getNetworkVlanProfile")]
+	[Get("/networks/{networkId}/vlanProfiles/{iname}")]
+	Task<VlanProfile> GetNetworkVlanProfileAsync(
+		string networkId,
+		string iname,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Update an existing VLAN profile of a network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="iname">The iname for the Vlan profile</param>
+	/// <param name="vlanProfileUpdateRequest">The vlan profile update request</param>
+	[ApiOperationId("updateNetworkVlanProfile")]
+	[Put("/networks/{networkId}/vlanProfiles/{iname}")]
+	Task<VlanProfile> UpdateNetworkVlanProfileAsync(
+		string networkId,
+		string iname,
+		[Body] VlanProfileUpdateRequest vlanProfileUpdateRequest,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Delete a VLAN profile of a network
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="networkId">The network id</param>
+	/// <param name="iname">The iname for the Vlan profile to delete</param>
+	[ApiOperationId("deleteNetworkVlanProfile")]
+	[Delete("/networks/{networkId}/vlanProfiles/{iname}")]
+	Task DeleteNetworkVlanProfileAsync(
+		string networkId,
+		string iname,
+		CancellationToken cancellationToken = default
+		);
+}
