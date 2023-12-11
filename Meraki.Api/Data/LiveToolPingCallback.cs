f492712022832a9d22153ec267ee46b23e172e72
Mathew Jones <mathew.jones@panoramicdata.com>
2023-12-11 16:06:50 +0000

    LiveToolPing callback added

---
+++ Meraki.Api/Data/LiveToolPingCallback.cs
@@ -0,0 +1,27 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Callback Status
+/// </summary>
+[DataContract]
+
+public class LiveToolPingCallback
+{
+	/// <summary>
+	/// id
+	/// </summary>
+	[DataMember(Name = "id")]
+	public string Id { get; set; } = string.Empty;
+
+	/// <summary>
+	/// url
+	/// </summary>
+	[DataMember(Name = "url")]
+	public string Url { get; set; } = string.Empty;
+
+	/// <summary>
+	/// status
+	/// </summary>
+	[DataMember(Name = "status")]
+	public string Status { get; set; } = string.Empty;
+}
