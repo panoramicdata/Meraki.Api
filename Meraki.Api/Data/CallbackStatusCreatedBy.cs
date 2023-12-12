918b1f8ecee2d51c9ab3f48e06d09c1b95d606b6
Mathew Jones <mathew.jones@panoramicdata.com>
2023-12-12 13:45:02 +0000

    Webhooks callback added

---
+++ Meraki.Api/Data/CallbackStatusCreatedBy.cs
@@ -0,0 +1,15 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// CallbackStatusCreatedBy
+/// </summary>
+[DataContract]
+public class CallbackStatusCreatedBy
+{
+	/// <summary>
+	/// The ID of the user who initiated the callback
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "adminId")]
+	public string AdminId { get; set; } = string.Empty;
+}
---
+++ Meraki.Api/Data/CallbackStatusWebhook.cs
@@ -0,0 +1,36 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// CallbackStatusWebhook
+/// </summary>
+[DataContract]
+public class CallbackStatusWebhook
+{
+	/// <summary>
+	/// The timestamp the callback was sent to the webhook receiver
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "sentAt")]
+	public string SentAt { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The webhook receiver URL where the callback will be sent
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "url")]
+	public string Url { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The webhook receiver used for the callback webhook
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "httpServer")]
+	public CallbackStatusWebhookHttpServer HttpServer { get; set; } = new();
+
+	/// <summary>
+	/// The payload template of the webhook used for the callback
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "payloadTemplate")]
+	public CallbackStatusWebhookPayloadTemplate PayloadTemplate { get; set; } = new();
+}
