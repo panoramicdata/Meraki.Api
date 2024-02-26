1095e3815fabfd6b70102605a568da8e9d9a6342
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-02-26 14:17:21 +0000

    Cleaned up loading config for unit tests

--- Meraki.Api.Test/MerakiClientTest.cs
+++ Meraki.Api.Test/MerakiClientTest.cs
@@ -13,44 +13,32 @@ public class MerakiClientTest(ITestOutputHelper _iTestOutputHelper) : IDisposabl
 
 	private MerakiClient? _merakiClient;
 
-	private TestConfig? _configuration;
 	private bool _disposedValue;
 
 	private readonly ICacheLogger _logger = _iTestOutputHelper.BuildLogger();
 
-	public TestConfig Configuration
+	public TestConfig Configuration { get; private set; }
+
+	private static TestConfig LoadConfig()
 	{
-		get
+		// Load config from file
+		var fileInfo = new FileInfo("../../../appsettings.json");
+
+		// Does the config file exist?
+		if (!fileInfo.Exists)
 		{
-			// Have we already created this?
-			if (_configuration != null)
-			{
-				// Yes - return that one
-				return _configuration;
-			}
-			// No - we need to create one
-
-			// Load config from file
-			var fileInfo = new FileInfo("../../../appsettings.json");
-
-			// Does the config file exist?
-			if (!fileInfo.Exists)
-			{
-				// No - hint to the user what to do
-				throw new ConfigurationException("Missing appsettings.json.  Please copy the appsettings.example.json in the project root folder and set the various values appropriately.");
-			}
-			// Yes
-
-			// Load in the config
-			_configuration = JsonConvert.DeserializeObject<TestConfig>(File.ReadAllText(fileInfo.FullName));
-			if (_configuration is null)
-			{
-				throw new ConfigurationException("Configuration did not deserialize");
-			}
-
-			_configuration.Validate();
-			return _configuration;
+			// No - hint to the user what to do
+			throw new ConfigurationException("Missing appsettings.json.  Please copy the appsettings.example.json in the project root folder and set the various values appropriately.");
 		}
+		// Yes
+
+		// Load in the config
+		var configuration = JsonConvert.DeserializeObject<TestConfig>(File.ReadAllText(fileInfo.FullName))
+			?? throw new ConfigurationException("Configuration did not deserialize");
+
+		configuration.Validate();
+
+		return configuration;
 	}
 
 	protected MerakiClient TestMerakiClient
@@ -61,6 +49,8 @@ public class MerakiClientTest(ITestOutputHelper _iTestOutputHelper) : IDisposabl
 			{
 				return _merakiClient;
 			}
+
+			Configuration = LoadConfig();
 
 			// Unit tests should always error if members are missing to aid picking up issues with models early
 			Configuration.MerakiClientOptions.JsonMissingMemberHandling = JsonMissingMemberHandling.ThrowOnError;
