a7970d6580a5e89892d8a690a36f19a40b34007d
Mathew Jones <mathew.jones@panoramicdata.com>
2024-03-28 15:43:27 +0000

    networks vlans enabled state

---
+++ Meraki.Api/Interfaces/General/Networks/INetworksVlans.cs
@@ -0,0 +1,15 @@
+﻿namespace Meraki.Api.Interfaces.General.Networks;
+
+public interface INetworksVlans
+{
+	/// <summary>
+	/// Returns the enabled status of VLANs for the network
+	/// </summary>
+	/// <param name="networkId">Network ID</param>
+	[ApiOperationId("getNetworkVlansEnabledState")]
+	[Get("/networks/{networkId}/vlansEnabledState")]
+	Task<NetworkVlansEnabledState> GetNetworkVlansEnabledStateAsync(
+		string networkId,
+		CancellationToken cancellationToken = default
+		);
+}
