61e9397978e0cd8af094fd1825ef57a7a0c304f4
Mathew Jones <mathew.jones@panoramicdata.com>
2024-04-18 14:33:40 +0100

    Organization Cloud Region Host Name added

--- Meraki.Api/Data/Region.cs
+++ Meraki.Api/Data/Region.cs
@@ -1,4 +1,12 @@
 ﻿namespace Meraki.Api.Data;
 
 [DataContract]
-public class Region : NamedItem;
+public class Region : NamedItem
+{
+	/// <summary>
+	/// Host - Undocumented 2024-04-18
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "host")]
+	public RegionHost Host { get; set; } = new();
+}
