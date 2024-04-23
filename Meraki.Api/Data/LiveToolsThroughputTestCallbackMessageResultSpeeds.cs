61fafd2c2a0e39ed5d94735c08cb3dcb841fd4f4
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-04-23 09:31:59 +0100

    Fixed payload

--- Meraki.Api/Data/LiveToolsThroughputTestCallbackMessageResultSpeeds.cs
+++ Meraki.Api/Data/LiveToolsThroughputTestCallbackMessageResultSpeeds.cs
@@ -3,14 +3,13 @@
 /// <summary>
 /// Live Tools Throughput Test Callback Payload Message ResultSpeeds
 /// </summary>
+[ApiAccessReadOnlyClass]
 [DataContract]
-
 public class LiveToolsThroughputTestCallbackMessageResultSpeeds
 {
 	/// <summary>
 	///  Shows the download speed from shard in (Mbps)
 	/// </summary>
-	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "downstream")]
 	public int? Downstream { get; set; }
 }
