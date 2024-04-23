826ce5bd828e8d20740cb52d32d4d4a6bad81110
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-04-23 09:11:07 +0100

    Updated ApiAccess

--- Meraki.Api/Data/AlertFiltersClient.cs
+++ Meraki.Api/Data/AlertFiltersClient.cs
@@ -6,12 +6,14 @@ public class AlertFiltersClient
 	/// <summary>
 	/// Client mac address
 	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "mac")]
 	public string? Mac { get; set; }
 
 	/// <summary>
 	/// Client name
 	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "name")]
 	public string? Name { get; set; }
 }
