83569751e08fd9697f935e3e45a52bb79d63d141
Mathew Jones <mathew.jones@panoramicdata.com>
2024-05-07 08:40:45 +0100

    Downstream from int to double

--- Meraki.Api/Data/LiveToolsThroughputTestResultSpeeds.cs
+++ Meraki.Api/Data/LiveToolsThroughputTestResultSpeeds.cs
@@ -12,6 +12,6 @@ public class LiveToolsThroughputTestResultSpeeds
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "downstream")]
-	public int Downstream { get; set; }
+	public double Downstream { get; set; }
 
 }
