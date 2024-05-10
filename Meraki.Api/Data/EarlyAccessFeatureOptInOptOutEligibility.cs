3903083dc66afd003f66fee965182ea47428f7d9
Mathew Jones <mathew.jones@panoramicdata.com>
2024-05-10 11:20:44 +0100

    Early Access Opt-Out Eligibility

---
+++ Meraki.Api/Data/EarlyAccessFeatureOptInOptOutEligibility.cs
@@ -0,0 +1,29 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Descriptions of the early access feature
+/// </summary>
+[DataContract]
+public class EarlyAccessFeatureOptInOptOutEligibility
+{
+	/// <summary>
+	/// User friendly message regarding opt-out eligibility
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "reason")]
+	public string Reason { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Condition flag to opt out from the feature
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "eligible")]
+	public bool Eligible { get; set; }
+
+	/// <summary>
+	/// Additional help information
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "help")]
+	public EarlyAccessFeatureOptInOptOutEligibilityHelp Help { get; set; } = new();
+}
---
+++ Meraki.Api/Data/EarlyAccessFeatureOptInOptOutEligibilityHelp.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Additional help information
+/// </summary>
+[DataContract]
+public class EarlyAccessFeatureOptInOptOutEligibilityHelp
+{
+	/// <summary>
+	/// Help link label
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "help")]
+	public string Help { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Help link url
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "url")]
+	public string Url { get; set; } = string.Empty;
+}
