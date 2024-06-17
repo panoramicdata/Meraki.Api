4fbab77b332276fcbeb041819741a179da44ce73
Mathew Jones <mathew.jones@panoramicdata.com>
2024-06-17 14:22:22 +0100

    Electronic Shelf Tag Endpoints added

---
+++ Meraki.Api/Interfaces/General/Networks/INetworkWirelessElectronicShelfLabel.cs
@@ -0,0 +1,28 @@
+﻿namespace Meraki.Api.Interfaces.General.Networks;
+	;
+public interface INetworkWirelessElectronicShelfLabel
+{
+	/// <summary>
+	/// Return the ESL settings of a wireless network
+	/// </summary>
+	/// <param name="networkId">Network Id</param>
+	/// <param name="cancellationToken"></param>
+	[ApiOperationId("getNetworkWirelessElectronicShelfLabel")]
+	[Get("/networks/{networkId}/wireless/electronicShelfLabel")]
+	Task<ElectronicShelfLabelSettingsNetwork> GetNetworkWirelessElectronicShelfLabelAsync(
+		string networkId,
+		CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Update the ESL settings of a wireless network
+	/// </summary>
+	/// <param name="networkId">Network Id</param>
+	/// <param name="settings">Update Settings</param>
+	/// <param name="cancellationToken"></param>
+	[ApiOperationId("updateNetworkWirelessElectronicShelfLabel")]
+	[Put("/networks/{networkId}/wireless/electronicShelfLabel")]
+	Task<ElectronicShelfLabelSettingsNetwork> UpdateNetworkWirelessElectronicShelfLabelAsync(
+		string networkId,
+		[Body] ElectronicShelfLabelSettingsNetwork settings,
+		CancellationToken cancellationToken = default);
+}
