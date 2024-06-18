59f8fda979fab54e2a834a092408635459d58921
Mathew Jones <mathew.jones@panoramicdata.com>
2024-06-18 18:03:53 +0100

    Camera resolutions updated

---
+++ Meraki.Api/Data/Mv63MResolution.cs
@@ -0,0 +1,20 @@
+namespace Meraki.Api.Data;
+
+/// <summary>
+/// Resolution of the camera.
+/// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+public enum Mv63MResolution
+{
+	/// <summary>
+	/// Enum 1920x1080
+	/// </summary>
+	[EnumMember(Value = "1920x1080")]
+	Size1920x1080,
+
+	/// <summary>
+	/// Enum 2688x1512
+	/// </summary>
+	[EnumMember(Value = "2688x1512")]
+	Size2688x1512
+}
