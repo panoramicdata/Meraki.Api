a42ee38c5f675037b8bb2f1340005655d1844ce8
Mathew Jones <mathew.jones@panoramicdata.com>
2024-06-18 12:53:24 +0100

    Filter Networks by ProductType

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsNetworks.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsNetworks.cs
@@ -10,6 +10,7 @@ public interface IOrganizationsNetworks
 	/// <param name="configTemplateId">An optional parameter that is the ID of a config template. Will return all networks bound to that template. (optional)</param>
 	/// <param name="tags">An optional parameter to filter networks by tags. The filtering is case-sensitive. If tags are included, 'tagsFilterType' should also be included (see below).</param>
 	/// <param name="tagsFilterType">An optional parameter of value 'withAnyTags' or 'withAllTags' to indicate whether to return networks which contain ANY or ALL of the included tags. If no type is included, 'withAnyTags' will be selected.</param>
+	/// <param name="productTypes">An optional parameter to filter networks by product type. Results will have at least one of the included product types.</param>
 	/// <param name="perPage">The number of entries per page returned. Acceptable range is 3 - 100000. Default is 1000.</param>
 	/// <param name="startingAfter">A token used by the server to indicate the start of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.</param>
 	/// <param name="endingBefore">A token used by the server to indicate the end of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.</param>
@@ -19,6 +20,7 @@ public interface IOrganizationsNetworks
 		string? configTemplateId = null,
 		[AliasAs("tags[]")] List<string>? tags = null,
 		string? tagsFilterType = null,
+		[AliasAs("productTypes[]")] List<ProductType>? productTypes,
 		int? perPage = 100000,
 		string? startingAfter = null,
 		string? endingBefore = null,
@@ -31,6 +33,7 @@ public interface IOrganizationsNetworks
 		string? configTemplateId = null,
 		[AliasAs("tags[]")] List<string>? tags = null,
 		string? tagsFilterType = null,
+		[AliasAs("productTypes[]")] List<ProductType>? productTypes,
 		string? startingAfter = null,
 		CancellationToken cancellationToken = default);
 
