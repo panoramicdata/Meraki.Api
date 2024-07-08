6168b68fb1cb9f5744dd123fa2e92c19fdad43f5
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-07-08 11:27:29 +0100

    DNAA-755 Fixed comment ordering

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsBandwidthUsageHistory.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsBandwidthUsageHistory.cs
@@ -10,14 +10,14 @@ public interface IOrganizationBandwidthUsageHistory
 	/// </summary>
 	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 	/// <param name="organizationId">The organization id</param>
+	/// <param name="t0">The beginning of the timespan for the data.</param>
+	/// <param name="t1">The end of the timespan for the data. t1 can be a maximum of 31 days after t0.</param>
+	/// <param name="timespan">The timespan for which the information will be fetched. If specifying timespan, do not specify parameters t0 and t1. The value must be in seconds and be less than or equal to 31 days. The default is 1 day.</param>
 	/// <param name="networkTag">Match result to an exact network tag</param>
 	/// <param name="deviceTag">Match result to an exact device tag</param>
 	/// <param name="networkId">Match result to an exact network id</param>
 	/// <param name="ssidName">Filter results by ssid name</param>
 	/// <param name="usageUplink">Filter results by usage uplink</param>
-	/// <param name="t0">The beginning of the timespan for the data.</param>
-	/// <param name="t1">The end of the timespan for the data. t1 can be a maximum of 31 days after t0.</param>
-	/// <param name="timespan">The timespan for which the information will be fetched. If specifying timespan, do not specify parameters t0 and t1. The value must be in seconds and be less than or equal to 31 days. The default is 1 day.</param>
 	[ApiOperationId("getOrganizationClientsBandwidthUsageHistory")]
 	[Get("/organizations/{organizationId}/clients/bandwidthUsageHistory")]
 	Task<List<BandwidthUsageHistory>> GetOrganizationClientsBandwidthUsageHistoryAsync(
