f91e5d708a87b605eb5b1c5efa606d167b4930a6
Mathew Jones <mathew.jones@panoramicdata.com>
2024-07-25 15:22:12 +0100

    added dot3az to switch template port

---
+++ Meraki.Api/Data/ConfigTemplateSwitchProfilePortDot3az.cs
@@ -0,0 +1,15 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// dot3az settings for the port
+/// </summary>
+[DataContract]
+public class ConfigTemplateSwitchProfilePortDot3az
+{
+	/// <summary>
+	/// The Energy Efficient Ethernet status of the switch template port.
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "dot3az")]
+	public bool? Dot3az { get; set; }
+}
