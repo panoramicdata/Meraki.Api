1ea28259488faecd40d20e6d5565c6e6dd03e336
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-08-04 22:22:15 +0100

    Added test

--- Meraki.Api.NewTest/ApiRequestsTests.cs
+++ Meraki.Api.NewTest/ApiRequestsTests.cs
@@ -1,8 +1,4 @@
-﻿using FluentAssertions;
-using Meraki.Api.Extensions;
-using System.Diagnostics;
-using System.Globalization;
-using Xunit.Abstractions;
+﻿using Xunit.Abstractions;
 
 namespace Meraki.Api.NewTest;
 [Collection("API Collection")]
@@ -14,26 +10,28 @@ public class ApiRequestsTests : MerakiClientUnitTest
 	{
 	}
 
-	[Fact]
-	public async Task GetApiRequests_Succeeds()
-	{
-		TestMerakiClient.Statistics.Reset();
-		var startTimeStamp = DateTime.Parse("2024-07-22 00:00:00Z", CultureInfo.InvariantCulture);
-		var endTimeStamp = startTimeStamp.AddDays(1);
-		var apiRequests = await TestMerakiClient
-			.Organizations
-			.ApiRequests
-			.GetOrganizationApiRequestsAllAsync(
-				"213309",
-				t0: startTimeStamp.Date.ToString("yyyy-MM-dd 00:00:00Z", CultureInfo.InvariantCulture),
-				t1: endTimeStamp.Date.ToString("yyyy-MM-dd 00:00:00Z", CultureInfo.InvariantCulture)
-				);
-		_ = apiRequests.Should().NotBeEmpty();
-		var json = System.Text.Json.JsonSerializer.Serialize(apiRequests, _jsonOptions);
-		// write the json to a file
-		var tempFile = Path.GetTempFileName();
-		await File.WriteAllTextAsync(tempFile, json);
-		Debug.WriteLine($"Json written to {tempFile}");
-		_ = json.Should().NotBeNullOrEmpty();
-	}
+	//[Fact]
+	//public async Task GetApiRequests_Succeeds()
+	//{
+	//	TestMerakiClient.Statistics.Reset();
+	//	var startTimeStamp = DateTime.Parse("2024-07-08 00:00:00Z", CultureInfo.InvariantCulture);
+	//	for (var i = 0; i < 14; i++)
+	//	{
+	//		startTimeStamp = startTimeStamp.AddDays(i);
+	//		var endTimeStamp = startTimeStamp.AddDays(1);
+	//		var apiRequests = await TestMerakiClient
+	//			.Organizations
+	//			.ApiRequests
+	//			.GetOrganizationApiRequestsAllAsync(
+	//				"xxxx",
+	//				t0: startTimeStamp.Date.ToString("yyyy-MM-dd 00:00:00Z", CultureInfo.InvariantCulture),
+	//				t1: endTimeStamp.Date.ToString("yyyy-MM-dd 00:00:00Z", CultureInfo.InvariantCulture)
+	//				);
+	//		var json = System.Text.Json.JsonSerializer.Serialize(apiRequests, _jsonOptions);
+	//		// write the json to a file
+	//		var outFile = Path.Combine(Path.GetTempPath(), $"apiRequests_{startTimeStamp:yyyyMMdd}.json");
+	//		await File.WriteAllTextAsync(outFile, json);
+	//		Debug.WriteLine($"Json written to {outFile}");
+	//	}
+	//}
 }
