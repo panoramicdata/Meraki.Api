2face9e0d20eb3f2a7983ae74c774770042cf56c
Mathew Jones <mathew.jones@panoramicdata.com>
2024-08-15 09:04:34 +0100

    Client Root CA Server Certificate Update

--- Meraki.Api/Data/ServerCaCertificate.cs
+++ Meraki.Api/Data/ServerCaCertificate.cs
@@ -11,5 +11,26 @@ public class ServerCaCertificate
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadUpdate)]
 	[DataMember(Name = "contents")]
-	public string Contents { get; set; } = string.Empty;
+	public string? Contents { get; set; }
+
+	/// <summary>
+	/// Name - Undocumented
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "name")]
+	public string? Name { get; set; }
+
+	/// <summary>
+	/// Expiration Date - Undocumented
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "expirationDate")]
+	public string? ExpirationDate { get; set; }
+
+	/// <summary>
+	/// Serial
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "serial")]
+	public string? Serial { get; set; }
 }
