a2d5e4b92679b170eb585097b608400078738c4e
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-08-19 13:11:15 +0100

    DNAA-769 503 is now treated the same was as a 502 and will retry

--- Meraki.Api/AuthenticatedBackingOffHttpClientHandler.cs
+++ Meraki.Api/AuthenticatedBackingOffHttpClientHandler.cs
@@ -112,6 +112,7 @@ internal sealed class AuthenticatedBackingOffHttpClientHandler(
 							);
 						break;
 					case 502:
+					case 503:
 						_logger.LogInformation(
 							"{LogPrefix}Received {StatusCodeInt} on attempt {AttemptCount}/{MaxAttemptCount}.",
 							logPrefix, statusCodeInt, attemptCount, _options.MaxAttemptCount
