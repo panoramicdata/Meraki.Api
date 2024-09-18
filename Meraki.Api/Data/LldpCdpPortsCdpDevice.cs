6a4041c8e0e646c9eb1107003eb49c6897f6f788
Mathew Jones <mathew.jones@panoramicdata.com>
2024-09-18 13:26:59 +0100

    LldpCdp model changed

---
+++ Meraki.Api/Data/LldpCdpPortsCdpDevice.cs
@@ -0,0 +1,16 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// LldpCdp Ports Device
+/// </summary>
+[DataContract]
+public class LldpCdpPortsDevice
+{
+	/// <summary>
+	/// Dashboard Url for the device
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "url")]
+	public string? Url { get; set; }
+
+}
