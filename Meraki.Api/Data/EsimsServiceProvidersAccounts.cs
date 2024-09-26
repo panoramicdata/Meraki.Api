a4815ae6bb1bfa594be904b7b044679ec6ab60de
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2024-09-26 14:33:10 +0100

    Converted to ItemsResponseWithMeta

--- Meraki.Api/Data/EsimsServiceProvidersAccounts.cs
+++ Meraki.Api/Data/EsimsServiceProvidersAccounts.cs
@@ -5,19 +5,11 @@ namespace Meraki.Api.Data;
 /// </summary>
 [DataContract]
 public class EsimsServiceProvidersAccounts
+	: ItemsResponseWithMeta<EsimsServiceProvidersAccountsItem>
 {
-	/// <summary>
-	///	Meta details about the result
-	/// </summary>
-	[ApiAccess(ApiAccess.Read)]
-	[DataMember(Name = "meta")]
-	public EsimsServiceProvidersAccountsMeta Meta { get; set; } = new();
-
 	/// <summary>
 	/// List of Cellular Service Provider Accounts
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
-	[DataMember(Name = "items")]
-	public List<EsimsServiceProvidersAccountsItem> Items { get; set; } = new();
-
+	public override List<EsimsServiceProvidersAccountsItem> Items { get; set; } = [];
 }
