1e3e711ca78026f363bccec1332b1a5d397a74fe
Mathew Jones <mathew.jones@panoramicdata.com>
2024-09-30 16:16:43 +0100

    Switch stack missing members added

---
+++ Meraki.Api/Data/SwitchStackMember.cs
@@ -0,0 +1,43 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// A switch stack member
+/// </summary>
+[DataContract]
+public class SwitchStackMember
+{
+	/// <summary>
+	/// Serial
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "serial	")]
+	public string Serial { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Name
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Model
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "model")]
+	public string Model { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Mac
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "mac")]
+	public string Mac { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Role
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "role")]
+	public string Role { get; set; } = string.Empty;
+}
