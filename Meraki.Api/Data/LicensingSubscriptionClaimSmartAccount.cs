d456a1c8049e8179a4b8cd17f8e68bbac4ce4cb8
Mathew Jones <mathew.jones@panoramicdata.com>
2025-01-08 11:13:07 +0000

    Subcription managemnent changes

--- Meraki.Api/Data/LicensingSubscriptionClaimSmartAccount.cs
+++ Meraki.Api/Data/LicensingSubscriptionClaimSmartAccount.cs
@@ -9,6 +9,7 @@ public class LicensingSubscriptionClaimSmartAccount
 	/// <summary>
 	/// Smart Account ID
 	/// </summary>
+	[Obsolete("Removed in Meraki API v1.53")]
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "id")]
 	public int Id { get; set; }
@@ -16,7 +17,22 @@ public class LicensingSubscriptionClaimSmartAccount
 	/// <summary>
 	/// The name of the smart account
 	/// </summary>
+	[Obsolete("Removed in Meraki API v1.53")]
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "name")]
 	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Subscription Smart Account status
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "status")]
+	public string Status { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Smart Account data
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "account")]
+	public LicensingSubscriptionClaimSmartAccountAccount Account { get; set; } = new();
 }
