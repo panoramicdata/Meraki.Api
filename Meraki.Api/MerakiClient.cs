368540360e1f936cc533030c082d015229e0a453
David Bond <david.bond@panoramicdata.com>
2025-03-09 14:01:10 +0000

    Improved documentation

--- Meraki.Api/MerakiClient.cs
+++ Meraki.Api/MerakiClient.cs
@@ -5,7 +5,48 @@ using Meraki.Api.Sections.Products.Licensing;
 namespace Meraki.Api;
 
 /// <summary>
-/// A Meraki Dashboard API client
+/// A Meraki Dashboard API client. This is your starting point for all API operations.
+/// Example usage:
+/// 
+/// ```csharp
+/// using Meraki.Api;
+/// using System;
+/// using System.Threading.Tasks;
+/// 
+/// namespace My.Project;
+/// public static class Program
+/// {
+///     public static async Task Main()
+///     {
+///         using var merakiClient = new MerakiClient(
+///             new MerakiClientOptions
+///             {
+///                 ApiKey = "0123456789abcdef0123456789abcdef01234567",
+///                 UserAgent = "YourProductName/YourProductVersion YourCompanyName"
+///             }
+///         );
+/// 
+///         var organizations = await merakiClient
+///             .Organizations
+///             .GetOrganizationsAsync()
+///             .ConfigureAwait(false);
+/// 
+///         var firstOrganization = organizations[0];
+/// 
+///         var devices = await merakiClient
+///             .Organizations
+///             .Devices
+///             .GetOrganizationDevicesAsync(firstOrganization.Id)
+///             .ConfigureAwait(false);
+/// 
+///         Console.WriteLine("Devices:");
+///         foreach (var device in devices)
+///         {
+///             Console.WriteLine($"    - {device.Serial}: {device.Name}");
+///         }
+///     }
+/// }
+/// ```
 /// </summary>
 public partial class MerakiClient : IDisposable
 {
