ba80b82820eb3dd6014256c740c7ee62f321ebed
Mathew Jones <mathew.jones@panoramicdata.com>
2025-04-09 15:39:13 +0100

    1.56 changes

---
+++ Meraki.Api/Data/L3InterfaceMode.cs
@@ -0,0 +1,26 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Optional parameter to filter L3 interfaces by mode. enum = ["loopback", "routed", "vlan"]
+/// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+public enum L3InterfaceMode
+{
+	/// <summary>
+	/// Enum Loopback for "loopback"
+	/// </summary>
+	[EnumMember(Value = "loopback")]
+	Loopback,
+
+	/// <summary>
+	/// Enum Routed for "routed"
+	/// </summary>
+	[EnumMember(Value = "routed")]
+	Routed,
+
+	/// <summary>
+	/// Enum Vlan for "vlan"
+	/// </summary>
+	[EnumMember(Value = "vlan")]
+	Vlan
+}
---
+++ Meraki.Api/Data/L3InterfaceProtocol.cs
@@ -0,0 +1,20 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Optional parameter to filter L3 interfaces by protocol.enum = ["ipv4", "ipv6"]
+/// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+public enum L3InterfaceProtocol
+{
+	/// <summary>
+	/// Enum Ipv4 for "ipv4"
+	/// </summary>
+	[EnumMember(Value = "ipv4")]
+	Ipv4,
+
+	/// <summary>
+	/// Enum Ipv6 for "ipv6"
+	/// </summary>
+	[EnumMember(Value = "ipv6")]
+	Ipv6
+}
