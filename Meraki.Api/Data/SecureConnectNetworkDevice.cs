97c0539a63709816eca4b89821502c60faa6e640
Mathew Jones <mathew.jones@panoramicdata.com>
2025-04-22 16:14:35 +0100

    SecureConnect Network Devices

---
+++ Meraki.Api/Data/SecureConnectNetworkDevice.cs
@@ -0,0 +1,59 @@
+using Meraki.Api.Interfaces.General.Devices;
+
+namespace Meraki.Api.Data;
+
+/// <summary>
+/// SecureConnectNetworkDevice
+/// </summary>
+[DataContract]
+public class SecureConnectNetworkDevice
+{
+	/// <summary>
+	/// The ID of the organization.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "id")]
+	public int? Id { get; set; }
+
+	/// <summary>
+	/// The unique global identifier for this traffic source (origin). Use the origin ID to manage the device.The origin ID is not used by the device.
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadUpdate)]
+	[DataMember(Name = "originId")]
+	public int? OriginId { get; set; }
+
+	/// <summary>
+	/// The time when the device was created. Specify an ISO 8601-formatted timestamp.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "createdAt")]
+	public string CreatedAt { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The unique identifier for the specific network device. Insert the identifier into the EDNS packets.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "deviceId")]
+	public string DeviceId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// A descriptive unique identifier for the device. Not used by the device.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "deviceKey")]
+	public string DeviceKey { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The name of the device.
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The serial number of the device.
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "serialNumber")]
+	public string SerialNumber { get; set; } = string.Empty;
+}
--- Meraki.Api/Interfaces/SecureConnect/Deployments/ISecureConnectDeploymentsNetworkDevices.cs
+++ Meraki.Api/Interfaces/SecureConnect/Deployments/ISecureConnectDeploymentsNetworkDevices.cs
@@ -2,4 +2,43 @@
 
 public interface ISecureConnectDeploymentsNetworkDevices
 {
+	/// <summary>
+	/// List the network devices.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">Organization ID associated with this resource.</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	[ApiOperationId("getAllNetworkDevices")]
+	[Get("/organizations/{organizationId}/networkDevices")]
+	Task<List<SecureConnectNetworkDevice>> GetAllNetworkDevicesAsync(
+		string organizationId,
+		CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Get a network device.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">Organization ID associated with this resource.</param>
+	/// <param name="originId">The origin ID of the network device. minimum = 1</param>"
+	/// <param name="cancellationToken">Cancellation token</param>
+	[ApiOperationId("getNetworkDevice")]
+	[Get("/organizations/{organizationId}/networkDevices/{originId}")]
+	Task<SecureConnectNetworkDevice> GetNetworkDeviceAsync(
+		string organizationId,
+		int originId,
+		CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Create a network device.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">Organization ID associated with this resource.</param>
+	/// <param name="secureConnectNetworkDeviceCreateRequest">The device creation request</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	[ApiOperationId("createNetworkDevice")]
+	[Get("/organizations/{organizationId}/networkDevices")]
+	Task<SecureConnectNetworkDevice> CreateNetworkDeviceAsync(
+		string organizationId,
+		[Body] SecureConnectNetworkDeviceCreateRequest secureConnectNetworkDeviceCreateRequest,
+		CancellationToken cancellationToken = default);
 }
