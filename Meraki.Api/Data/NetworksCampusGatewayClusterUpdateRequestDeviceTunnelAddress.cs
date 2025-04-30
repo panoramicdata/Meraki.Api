c4b291dbde460d8d654faf310686a93131ffdf5c
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-04-30 15:41:10 +0100

    Added  updateNetworkCampusGatewayCluster method and models

---
+++ Meraki.Api/Data/NetworksCampusGatewayClusterUpdateRequestDeviceTunnelAddress.cs
@@ -0,0 +1,20 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Tunnel IP addresses of the device
+/// </summary>
+[DataContract]
+public class NetworksCampusGatewayClusterUpdateRequestDeviceTunnelAddress
+{
+	/// <summary>
+	/// IP address of the interface
+	/// </summary>
+	[DataMember(Name = "address")]
+	public string Address { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Protocol of the interface
+	/// </summary>
+	[DataMember(Name = "protocol")]
+	public NetworksCampusGatewayClusterProtocol Protocol { get; set; }
+}
--- Meraki.Api/Interfaces/General/Networks/INetworksCampusGateway.cs
+++ Meraki.Api/Interfaces/General/Networks/INetworksCampusGateway.cs
@@ -14,4 +14,20 @@ public interface INetworksCampusGateway
 		[Body] NetworksCampusGatewayClusterRequest campusGatewayCluster,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Update a cluster and add/remove campus gateways to/from it
+	/// </summary>
+	/// <param name="networkId">The Network ID</param>
+	/// <param name="clusterId">The Cluster ID</param>
+	/// <param name="campusGatewayCluster"></param>
+	/// <param name="cancellationToken">Cancellation Token</param>
+	/// <returns></returns>
+	[Put("/networks/{networkId}/campusGateway/clusters/{clusterId}")]
+	public Task<NetworksCampusGatewayClusterResponse> UpdateCampusGatewayClusterAsync(
+		string networkId,
+		string clusterId,
+		[Body] NetworksCampusGatewayClusterUpdateRequest campusGatewayCluster,
+		CancellationToken cancellationToken = default
+	);
 }
