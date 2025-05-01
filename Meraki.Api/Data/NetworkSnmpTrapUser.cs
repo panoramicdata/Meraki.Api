4e14e4e8f0aa5ad8d5e4829f6c7660b6572e1afc
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-01 12:28:46 +0100

    Added updateNetworkSnmpTraps method

---
+++ Meraki.Api/Data/NetworkSnmpTrapUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// An SNMP trap user
+/// </summary>
+[DataContract]
+public class NetworkSnmpTrapUser
+{
+	/// <summary>
+	/// The name of the SNMP trap user
+	/// </summary>
+	[DataMember(Name = "name")]
+	public string? Name { get; set; }
+
+	/// <summary>
+	/// The passphrase for the SNMP trap user
+	/// </summary>
+	[DataMember(Name = "passphrase")]
+	public string? Passphrase { get; set; }
+}
---
+++ Meraki.Api/Data/NetworkSnmpTrapV3Response.cs
@@ -0,0 +1,15 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Configuration details for SNMP v3
+/// </summary>
+[DataContract]
+public class NetworkSnmpTrapV3Response
+{
+	/// <summary>
+	/// An array of users for SNMP v3
+	/// </summary>
+	[DataMember(Name = "users")]
+	public List<NetworkSnmpTrapV3ResponseUser>? Users { get; set; }
+
+}
