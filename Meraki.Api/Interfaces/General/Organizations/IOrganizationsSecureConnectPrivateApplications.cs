24ede7c0a1947225f348fd667a03e33eceef5ca2
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-09 11:39:59 +0100

    Added createOrganizationSecureConnectPrivateApplication method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateApplications.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateApplications.cs
@@ -14,4 +14,20 @@ public interface IOrganizationsSecureConnectPrivateApplications
 		string organizationId,
 		CancellationToken cancellationToken = default
 		);
+
+	/// <summary>
+	/// Adds a new private application to the Organization. A maximum of 300 private applications are allowed for an organization.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="secureConnectPrivateApplication"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationSecureConnectPrivateApplication")]
+	[Post("/organizations/{organizationId}/secureConnect/privateApplications")]
+	Task<SecureConnectPrivateApplication> CreateOrganizationSecureConnectPrivateApplicationAsync(
+		string organizationId,
+		[Body] SecureConnectPrivateApplicationCreateRequest secureConnectPrivateApplication,
+		CancellationToken cancellationToken = default
+		);
 }
