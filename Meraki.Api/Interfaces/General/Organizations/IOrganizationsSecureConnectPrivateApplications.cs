4518045c5d5b1d3a7ea7811083ca79992a3370e6
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-09 11:53:31 +0100

    Added updateOrganizationSecureConnectPrivateApplication method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateApplications.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateApplications.cs
@@ -42,4 +42,18 @@ public interface IOrganizationsSecureConnectPrivateApplications
 	[ApiOperationId("getOrganizationSecureConnectPrivateApplication")]
 	[Get("/organizations/{organizationId}/secureConnect/privateApplications/{id}")]
 	Task<SecureConnectPrivateApplication> GetOrganizationSecureConnectPrivateApplicationAsync(string organizationId, string id, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Updates a specific private application. Updates can be made to Name, Description, Destinations, App Protocol, SNI and SSL verification. Application groups can be added or removed.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="id"></param>
+	/// <param name="secureConnectPrivateApplication"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("updateOrganizationSecureConnectPrivateApplication")]
+	[Put("/organizations/{organizationId}/secureConnect/privateApplications/{id}")]
+	Task<SecureConnectPrivateApplication> UpdateOrganizationSecureConnectPrivateApplicationAsync(string organizationId, string id, [Body] SecureConnectPrivateApplicationUpdateRequest secureConnectPrivateApplication, CancellationToken cancellationToken = default
+		);
 }
