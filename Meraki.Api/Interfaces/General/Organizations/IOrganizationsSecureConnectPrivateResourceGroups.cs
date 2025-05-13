32fd5f14e543df96e6de7e3715e769536317c694
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-13 12:15:34 +0100

    Added createOrganizationSecureConnectPrivateResourceGroup method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateResourceGroups.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSecureConnectPrivateResourceGroups.cs
@@ -14,4 +14,20 @@ public interface IOrganizationsSecureConnectPrivateResourceGroups
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Adds a new private resource group to an organization.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationSecureConnectPrivateResourceGroup")]
+	[Post("/organizations/{organizationId}/secureConnect/privateResourceGroups")]
+	Task<SecureConnectPrivateResourceGroupsResponse> CreateOrganizationSecureConnectPrivateResourceGroupAsync(
+		string organizationId,
+		SecureConnectPrivateResourceGroupCreateRequest request,
+		CancellationToken cancellationToken = default
+	);
 }
