27b6e436ee1dbad8e757c138d96a053eb068d318
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-13 15:57:53 +0100

    Added createOrganizationSmAdminsRole method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSmAdmins.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSmAdmins.cs
@@ -14,4 +14,20 @@ public interface IOrganizationsSmAdmins
 		string organizationId,
 		CancellationToken cancellationToken = default
 		);
+
+	/// <summary>
+	/// Create a Limited Access Role
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="organizationSmAdminsRole"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationSmAdminsRole")]
+	[Post("/organizations/{organizationId}/sm/admins/roles")]
+	Task<OrganizationSmAdminsRole> CreateOrganizationSmAdminsRoleAsync(
+		string organizationId,
+		[Body] OrganizationSmAdminsRoleCreateRequest organizationSmAdminsRole,
+		CancellationToken cancellationToken = default
+		);
 }
