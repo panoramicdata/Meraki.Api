7a6b1cba550cf3a9aa2b9753e60b4c77adc84546
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-14 15:43:15 +0100

    Added getOrganizationCertificatesRadSecDeviceCertificateAuthorities method and models

--- Meraki.Api/Sections/General/Organizations/OrganizationsCertificatesSection.cs
+++ Meraki.Api/Sections/General/Organizations/OrganizationsCertificatesSection.cs
@@ -2,5 +2,7 @@
 public partial class OrganizationsCertificatesSection
 {
 	[RefitPromoteCalls]
-	public IOrganizationsCertificates Certificates { get; set; } = null!;
+	public IOrganizationsCertificates Certificates { get; internal set; } = null!;
+
+	public OrganizationsCertificatesRadSecSection RadSec { get; internal set; } = null!;
 }
