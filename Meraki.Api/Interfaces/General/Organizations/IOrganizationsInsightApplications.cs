ee091019cdfbe410ce8b5abb78e34855fd40e76a
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-14 11:38:24 +0100

    Added updateOrganizationInsightApplication method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsInsightApplications.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsInsightApplications.cs
@@ -17,4 +17,22 @@ public interface IOrganizationsInsightApplications
 		[Body] OrganizationInsightApplicationCreateRequest request,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Update an Insight tracked application
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="applicationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("updateOrganizationInsightApplication")]
+	[Put("/organizations/{organizationId}/insight/applications/{applicationId}")]
+	Task<OrganizationInsightApplication> UpdateOrganizationInsightApplicationAsync(
+		string organizationId,
+		string applicationId,
+		[Body] OrganizationInsightApplicationUpdateRequest request,
+		CancellationToken cancellationToken = default
+	);
 }
