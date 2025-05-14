3b16f89d46525b4705f7d2f8012c624f1995d966
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-14 10:38:43 +0100

    Added createOrganizationInsightWebApp method and models

---
+++ Meraki.Api/Data/OrganizationInsightWebAppCreateResponse.cs
@@ -0,0 +1,50 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Organization Insight Web Application Create Response
+/// </summary>
+[DataContract]
+public class OrganizationInsightWebAppCreateResponse
+{
+	/// <summary>
+	/// Category type for web application
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "category")]
+	public string Category { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Counter set rule id
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "counterSetRuleId")]
+	public string CounterSetRuleId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Http hostname for tracking web app
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "expression")]
+	public string Expression { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Web Application name
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// If custom host or port is specified, this field populates
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "signature")]
+	public OrganizationInsightWebAppCreateResponseSignature Signature { get; set; } = new();
+
+	/// <summary>
+	/// Default thresholds for each application
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "thresholds")]
+	public OrganizationInsightWebAppThresholds Thresholds { get; set; } = new();
+}
--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsInsightWebApps.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsInsightWebApps.cs
@@ -14,4 +14,20 @@ public interface IOrganizationsInsightWebApps
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Add a custom web application for Insight to be able to track
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationInsightWebApp")]
+	[Post("/organizations/{organizationId}/insight/webApps")]
+	Task<OrganizationInsightWebAppCreateResponse> CreateOrganizationInsightWebAppAsync(
+		string organizationId,
+		[Body] OrganizationInsightWebAppCreateRequest request,
+		CancellationToken cancellationToken = default
+	);
 }
