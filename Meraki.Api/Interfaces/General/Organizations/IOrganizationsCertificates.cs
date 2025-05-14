eae4a29eaebacfb260e4063b8ec66f55f4eaafaf
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-14 15:15:10 +0100

    Added deleteOrganizationCertificate method

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsCertificates.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsCertificates.cs
@@ -25,4 +25,16 @@ public interface IOrganizationsCertificates
 	[ApiOperationId("updateOrganizationCertificate")]
 	[Put("/organizations/{organizationId}/certificates/{certificateId}")]
 	Task<OrganizationCertificates> UpdateOrganizationCertificateAsync(string organizationId, string certificateId, OrganizationCertificatesUpdateRequest request, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Delete a certificate for an organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="certificateId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("deleteOrganizationCertificate")]
+	[Delete("/organizations/{organizationId}/certificates/{certificateId}")]
+	Task DeleteOrganizationCertificateAsync(string organizationId, string certificateId, CancellationToken cancellationToken = default);
 }
