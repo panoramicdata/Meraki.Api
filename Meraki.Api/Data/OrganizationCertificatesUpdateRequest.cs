1a01fb965f81bb14dcec139b59aa46f0cf64eb5f
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-14 15:13:46 +0100

    Added updateOrganizationCertificate method and models

---
+++ Meraki.Api/Data/OrganizationCertificatesUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Organization Certificates Update Request
+/// </summary>
+[DataContract]
+public class OrganizationCertificatesUpdateRequest
+{
+	/// <summary>
+	/// Description of a certificate that already exist in your org
+	/// </summary>
+	[DataMember(Name = "description")]
+	public string? Description { get; set; }
+}
--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsCertificates.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsCertificates.cs
@@ -12,4 +12,17 @@ public interface IOrganizationsCertificates
 	[ApiOperationId("getOrganizationCertificates")]
 	[Get("/organizations/{organizationId}/certificates")]
 	Task<OrganizationCertificates> GetOrganizationCertificatesAsync(string organizationId, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Update a certificate's description for an organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="certificateId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("updateOrganizationCertificate")]
+	[Put("/organizations/{organizationId}/certificates/{certificateId}")]
+	Task<OrganizationCertificates> UpdateOrganizationCertificateAsync(string organizationId, string certificateId, OrganizationCertificatesUpdateRequest request, CancellationToken cancellationToken = default);
 }
