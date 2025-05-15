58c7eed1751624b78e41d014999d06964471141b
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-15 15:57:56 +0100

    Added getOrganizationAuthRadiusServers method and models

---
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsAuthRadius.cs
@@ -0,0 +1,17 @@
+﻿namespace Meraki.Api.Interfaces.General.Organizations;
+public interface IOrganizationsAuthRadius
+{
+	/// <summary>
+	/// List the organization-wide RADIUS servers in the organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getOrganizationAuthRadiusServers")]
+	[Get("/organizations/{organizationId}/auth/radius/servers")]
+	Task<List<OrganizationsAuthRadiusServer>> GetOrganizationAuthRadiusServersAsync(
+		string organizationId,
+		CancellationToken cancellationToken = default
+	);
+}
