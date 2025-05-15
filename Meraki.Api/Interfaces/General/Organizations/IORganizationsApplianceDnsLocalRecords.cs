01938b084049edb46810f74311c50f218046d866
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-15 10:16:36 +0100

    Added createOrganizationApplianceDnsLocalRecord method and models

--- Meraki.Api/Interfaces/General/Organizations/IORganizationsApplianceDnsLocalRecords.cs
+++ Meraki.Api/Interfaces/General/Organizations/IORganizationsApplianceDnsLocalRecords.cs
@@ -13,4 +13,19 @@ public interface IOrganizationsApplianceDnsLocalRecords
 	Task<OrganizationApplianceDnsLocalRecordsResponse> GetOrganizationApplianceDnsLocalRecordsAsync(
 		string organizationId,
 		CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Create a new local DNS record
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationApplianceDnsLocalRecord")]
+	[Post("/organizations/{organizationId}/appliance/dns/local/records")]
+	Task<OrganizationApplianceDnsLocalRecordsResponse> CreateOrganizationApplianceDnsLocalRecordAsync(
+		string organizationId,
+		OrganizationApplianceDnsLocalRecordsCreateRequest request,
+		CancellationToken cancellationToken = default);
 }
