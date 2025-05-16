b55f32759f692e3489594a14266f41fb7bcd4d77
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-16 10:13:59 +0100

    Added getOrganizationDevicesControllerMigrations method and models

---
+++ Meraki.Api/Data/OrganizationDevicesControllerMigrationsResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Organization Devices Controller Migrations Response
+/// </summary>
+[DataContract]
+public class OrganizationDevicesControllerMigrationsResponse
+{
+	/// <summary>
+	/// Metadata relevant to the paginated dataset
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "meta")]
+	public OrganizationDevicesControllerMigrationsMetadata Meta { get; set; } = new();
+
+	/// <summary>
+	/// List of migrations for the specified devices
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "items")]
+	public List<OrganizationsDevicesControllerMigration> Items { get; set; } = [];
+}
--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsDevicesController.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsDevicesController.cs
@@ -16,4 +16,18 @@ public interface IOrganizationsDevicesController
 		[Body] OrganizationsDevicesControllerMigrationCreateRequest request,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Retrieve device migration statuses in an organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getOrganizationDevicesControllerMigrations")]
+	[Get("/organizations/{organizationId}/devices/controller/migrations")]
+	Task<OrganizationDevicesControllerMigrationsResponse> GetOrganizationDevicesControllerMigrationsAsync(
+		string organizationId,
+		CancellationToken cancellationToken = default
+	);
 }
