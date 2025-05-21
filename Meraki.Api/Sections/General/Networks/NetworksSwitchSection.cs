c867e059fa8b72e051a562f8eae10291f38310e8
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-21 12:34:28 +0100

    Added getNetworkSwitchDhcpServerPolicyArpInspectionWarningsByDevice method and models

--- Meraki.Api/Sections/General/Networks/NetworksSwitchSection.cs
+++ Meraki.Api/Sections/General/Networks/NetworksSwitchSection.cs
@@ -4,4 +4,6 @@ public class NetworksSwitchSection
 	public INetworksSwitchPorts Ports { get; internal set; } = null!;
 
 	public INetworksSwitchDhcp Dhcp { get; internal set; } = null!;
+
+	public INetworksSwitchDhcpServerPolicy DhcpServerPolicy { get; internal set; } = null!;
 }
