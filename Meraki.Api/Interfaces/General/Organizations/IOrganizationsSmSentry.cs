2bd3260f56a5a90884434a3f595176fa95d492ca
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-21 10:24:40 +0100

    Added updateOrganizationSmSentryPoliciesAssignments method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSmSentry.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSmSentry.cs
@@ -15,4 +15,20 @@ public interface IOrganizationsSmSentry
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Update an Organizations Sentry Policies using the provided list. Sentry Policies are ordered in descending order of priority (i.e. highest priority at the bottom, this is opposite the Dashboard UI). Policies not present in the request will be deleted.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("updateOrganizationSmSentryPoliciesAssignments")]
+	[Put("/organizations/{organizationId}/sm/sentry/policies/assignments")]
+	Task<OrganizationSmSentryPoliciesAssignmentsResponse> UpdateOrganizationSmSentryPoliciesAssignments(
+		string organizationId,
+		[Body] OrganizationSmSentryPoliciesAssignmentsUpdateRequest request,
+		CancellationToken cancellationToken = default
+	);
 }
