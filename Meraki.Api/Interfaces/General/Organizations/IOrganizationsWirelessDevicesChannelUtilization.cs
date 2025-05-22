009b48696220ec577455f6b66a6e57d5482b76b8
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-22 12:53:49 +0100

    Added getOrganizationWirelessDevicesChannelUtilizationHistoryByDeviceByInterval method and models

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsWirelessDevicesChannelUtilization.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsWirelessDevicesChannelUtilization.cs
@@ -24,4 +24,17 @@ public interface IOrganizationsWirelessDevicesChannelUtilization
 	[ApiOperationId("getOrganizationWirelessDevicesChannelUtilizationByNetwork")]
 	[Get("/organizations/{organizationId}/wireless/devices/channelUtilization/byNetwork")]
 	Task<List<OrganizationWirelessDevicesChannelUtilizationByNetworkItem>> GetOrganizationWirelessDevicesChannelUtilizationsByNetworkAsync(string orgnanizationId, string networkId, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Get a time-series of average channel utilization for all bands, segmented by device.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="orgnanizationId"></param>
+	/// <param name="networkId"></param>
+	/// <param name="interval"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getOrganizationWirelessDevicesChannelUtilizationHistoryByDeviceByInterval")]
+	[Get("/organizations/{organizationId}/wireless/devices/channelUtilization/history/byDevice/byInterval")]
+	Task<List<OrganizationWirelessDevicesChannelUtilizationHistoryByDeviceByIntervalItem>> GetOrganizationWirelessDevicesChannelUtilizationHistoryByDeviceByIntervalAsync(string orgnanizationId, string networkId, string interval, CancellationToken cancellationToken = default);
 }
