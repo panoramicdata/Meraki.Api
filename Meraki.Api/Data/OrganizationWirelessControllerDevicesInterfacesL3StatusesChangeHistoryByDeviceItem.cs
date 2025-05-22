1d7d1c3aa855fc7121ee93c266e59d2bbcdb1ccd
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-05-22 16:47:36 +0100

    Added getOrganizationWirelessControllerDevicesInterfacesL3StatusesChangeHistoryByDevice method and models

---
+++ Meraki.Api/Data/OrganizationWirelessControllerDevicesInterfacesL3StatusesChangeHistoryByDeviceItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Wireless LAN controller layer 3 interface historical status
+/// </summary>
+[DataContract]
+public class OrganizationWirelessControllerDevicesInterfacesL3StatusesChangeHistoryByDeviceItem
+{
+	/// <summary>
+	/// The cloud ID of the wireless LAN controller
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "serial")]
+	public string Serial { get; set; } = string.Empty;
+
+	/// <summary>
+	/// layer 3 interfaces belongs to the wireless LAN controller
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "interfaces")]
+	public List<OrganizationWirelessControllerDevicesInterfacesL3StatusesChangeHistoryByDeviceItemInterface> Interfaces { get; set; } = [];
+}
