60fd3d7f01e2c49b173dd31a1efc214e99982f15
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-02 12:57:12 +0100

    Added getDeviceWirelessHealthScores method and models

--- Meraki.Api/Sections/General/Devices/DevicesWirelessSection.cs
+++ Meraki.Api/Sections/General/Devices/DevicesWirelessSection.cs
@@ -1,6 +1,9 @@
 ﻿namespace Meraki.Api.Sections.General.Devices;
-public class DevicesWirelessSection
+public partial class DevicesWirelessSection
 {
+	[RefitPromoteCalls]
+	internal IDevicesWireless DevicesWireless { get; set; } = null!;
+
 	public IDeviceWirelessAlternateManagementInterface AlternateManagementInterface { get; internal set; } = null!;
 
 	public DevicesWirelessRadioSection Radio { get; internal set; } = null!;
