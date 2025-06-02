1a36a66c4194f81a6015bd5b9cffe29d97949806
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-02 12:27:34 +0100

    Added getDeviceAppliancePrefixesDelegated method and models

---
+++ Meraki.Api/Interfaces/General/Devices/IDevicesAppliancePrefixes.cs
@@ -0,0 +1,13 @@
+﻿namespace Meraki.Api.Interfaces.General.Devices;
+public interface IDevicesAppliancePrefixes
+{
+	/// <summary>
+	/// Return current delegated IPv6 prefixes on an appliance.
+	/// </summary>
+	/// <param name="serial"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getDeviceAppliancePrefixesDelegated")]
+	[Get("/devices/{serial}/appliance/prefixes/delegated")]
+	Task<List<DeviceAppliancePrefixesDelegatedItem>> GetDeviceAppliancePrefixesDelegatedAsync(string serial, CancellationToken cancellationToken = default);
+}
