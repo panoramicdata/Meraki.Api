b326dcc3fc0594ab5fef7b2c8d7213b7cd3a3373
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-02 12:43:36 +0100

    Added getDeviceAppliancePrefixesDelegatedVlanAssignments method and models

--- Meraki.Api/Interfaces/General/Devices/IDevicesAppliancePrefixes.cs
+++ Meraki.Api/Interfaces/General/Devices/IDevicesAppliancePrefixes.cs
@@ -4,10 +4,22 @@ public interface IDevicesAppliancePrefixes
 	/// <summary>
 	/// Return current delegated IPv6 prefixes on an appliance.
 	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 	/// <param name="serial"></param>
 	/// <param name="cancellationToken"></param>
 	/// <returns></returns>
 	[ApiOperationId("getDeviceAppliancePrefixesDelegated")]
 	[Get("/devices/{serial}/appliance/prefixes/delegated")]
 	Task<List<DeviceAppliancePrefixesDelegatedItem>> GetDeviceAppliancePrefixesDelegatedAsync(string serial, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Return prefixes assigned to all IPv6 enabled VLANs on an appliance.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="serial"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getDeviceAppliancePrefixesDelegatedVlanAssignments")]
+	[Get("/devices/{serial}/appliance/prefixes/delegated/vlanAssignments")]
+	Task<List<DeviceAppliancePrefixesDelegatedVlanAssignmentItem>> GetDeviceAppliancePrefixesDelegatedVlanAssignmentsAsync(string serial, CancellationToken cancellationToken = default);
 }
