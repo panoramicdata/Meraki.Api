6349a7665c83153a59013569b534f0a5244e2a1c
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-04 09:54:30 +0100

    Added getOrganizationCameraPermission method

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsCameraPermissions.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsCameraPermissions.cs
@@ -15,4 +15,20 @@ public interface IOrganizationsCameraPermissions
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Retrieve a single permission scope
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="permissionScopeId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getOrganizationCameraPermission")]
+	[Get("/organizations/{organizationId}/camera/permissions/{permissionScopeId}")]
+	Task<OrganizationCameraPermission> GetOrganizationCameraPermissionAsync(
+		string organizationId,
+		string permissionScopeId,
+		CancellationToken cancellationToken = default
+	);
 }
