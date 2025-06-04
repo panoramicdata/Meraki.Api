6f2279085db822bf859ce659c04efaaa07e18aad
Peter Acs <peter.acs@panoramicdata.com>
2025-06-04 18:16:37 +0200

    Added missing properties to LldpCdpPortsLldp

--- Meraki.Api/Data/LldpCdpPortsCdpLldp.cs
+++ Meraki.Api/Data/LldpCdpPortsCdpLldp.cs
@@ -33,4 +33,46 @@ public class LldpCdpPortsLldp
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "systemName")]
 	public string? SystemName { get; set; }
+
+	/// <summary>
+	/// Device system description
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "systemDescription")]
+	public string? SystemDescription { get; set; }
+
+	/// <summary>
+	/// Port description
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "portDescription")]
+	public string? PortDescription { get; set; }
+
+	/// <summary>
+	/// Chassis ID
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "chassisId")]
+	public string? ChassisId { get; set; }
+
+	/// <summary>
+	/// Management VLAN
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "managementVlan")]
+	public int? ManagementVlan { get; set; }
+
+	/// <summary>
+	/// Port VLAN
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "portVlan")]
+	public int? PortVlan { get; set; }
+
+	/// <summary>
+	/// System capabilities
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "systemCapabilities")]
+	public string? SystemCapabilities { get; set; }
 }
