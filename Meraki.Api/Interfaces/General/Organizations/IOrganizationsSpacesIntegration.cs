0dacac307348505d645e8d334a06898505054a02
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-05 14:15:59 +0100

    Added removeOrganizationSpacesIntegration method

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSpacesIntegration.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSpacesIntegration.cs
@@ -12,4 +12,15 @@ public interface IOrganizationsSpacesIntegration
 	[ApiOperationId("deleteOrganizationSpacesIntegrationRemove")]
 	[Delete("/organizations/{organizationId}/spaces/integration/remove")]
 	Task<OrganizationSpacesIntegrationActionResponse> DeleteOrganizationSpacesIntegrationRemoveAsync(string organizationId, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Remove the Spaces integration from Meraki
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("removeOrganizationSpacesIntegration")]
+	[Post("/organizations/{organizationId}/spaces/integration/remove")]
+	Task<OrganizationSpacesIntegrationActionResponse> RemoveOrganizationSpacesIntegrationAsync(string organizationId, CancellationToken cancellationToken = default);
 }
