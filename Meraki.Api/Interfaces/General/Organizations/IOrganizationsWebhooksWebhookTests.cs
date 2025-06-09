bfb606c59fed94a4a4f9fd65c7c10809cace0053
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-09 11:58:33 +0100

    Added createOrganizationWebhooksWebhookTest method and models

---
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsWebhooksWebhookTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Meraki.Api.Interfaces.General.Organizations;
+public interface IOrganizationsWebhooksWebhookTests
+{
+	/// <summary>
+	/// Send a test webhook for an organization
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createOrganizationWebhooksWebhookTest")]
+	[Post("/organizations/{organizationId}/webhooks/webhookTests")]
+	Task<OrganizationWebhookTest> CreateOrganizationWebhooksWebhookTestAsync(
+		string organizationId,
+		[Body] OrganizationWebhookTestCreateRequest request,
+		CancellationToken cancellationToken = default
+	);
+}
