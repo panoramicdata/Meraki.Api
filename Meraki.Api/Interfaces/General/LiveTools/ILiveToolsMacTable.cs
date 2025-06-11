85b72cee82edd473d8634380e5259f0d51aaec54
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 17:41:52 +0100

    Added getDeviceLiveToolsMacTable method and models

---
+++ Meraki.Api/Interfaces/General/LiveTools/ILiveToolsMacTable.cs
@@ -0,0 +1,19 @@
+﻿namespace Meraki.Api.Interfaces.General.LiveTools;
+public interface ILiveToolsMacTable
+{
+	/// <summary>
+	/// Return a MAC table live tool job.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="serial"></param>
+	/// <param name="macTableId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("getDeviceLiveToolsMacTable")]
+	[Get("/devices/{serial}/liveTools/macTable/{macTableId}")]
+	Task<DeviceLiveToolsMacTableGetResponse> GetDeviceLiveToolsMacTableAsync(
+		string serial,
+		string macTableId,
+		CancellationToken cancellationToken = default
+	);
+}
