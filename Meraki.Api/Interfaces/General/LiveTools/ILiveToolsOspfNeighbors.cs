8dfa6547b0052a351ab0cb013bfb7af0cd0b44c9
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 16:50:33 +0100

    Added createDeviceLiveToolsOspfNeighbor method and models

--- Meraki.Api/Interfaces/General/LiveTools/ILiveToolsOspfNeighbors.cs
+++ Meraki.Api/Interfaces/General/LiveTools/ILiveToolsOspfNeighbors.cs
@@ -17,4 +17,20 @@ public interface ILiveToolsOspfNeighbors
 		string ospfNeighborsId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Enqueue a job to perform a OSPF neighbors request for the device. This endpoint currently supports switches. This endpoint has a sustained rate limit of one request every five seconds per device, with an allowed burst of five requests.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="serial"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createDeviceLiveToolsOspfNeighbor")]
+	[Post("/devices/{serial}/liveTools/ospfNeighbors")]
+	Task<DeviceLiveToolsOspfNeighborCreateResponse> CreateDeviceLiveToolsOspfNeighborAsync(
+		string serial,
+		[Body] DeviceLiveToolsOspfNeighborCreateRequest request,
+		CancellationToken cancellationToken = default
+	);
 }
