f66ad7d126611937f822cf4c94e85f7a589edf80
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 17:32:29 +0100

    Added createDeviceLiveToolsMulticastRouting method and models

--- Meraki.Api/Interfaces/General/LiveTools/ILiveToolsMulticastRouting.cs
+++ Meraki.Api/Interfaces/General/LiveTools/ILiveToolsMulticastRouting.cs
@@ -5,6 +5,7 @@ public interface ILiveToolsMulticastRouting
 	/// <summary>
 	/// Return a Multicast routing live tool job.
 	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
 	/// <param name="serial"></param>
 	/// <param name="multicastRoutingId"></param>
 	/// <param name="cancellationToken"></param>
@@ -14,6 +15,22 @@ public interface ILiveToolsMulticastRouting
 	Task<DeviceLiveToolsMulticastRoutingGetResponse> GetDeviceLiveToolsMulticastRoutingAsync(
 		string serial,
 		string multicastRoutingId,
+		CancellationToken cancellationToken = default
+	);
+
+	/// <summary>
+	/// Enqueue a job to perform a Multicast routing request for the device. This endpoint currently supports switches. This endpoint has a sustained rate limit of one request every five seconds per device, with an allowed burst of five requests.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="serial"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createDeviceLiveToolsMulticastRouting")]
+	[Post("/devices/{serial}/liveTools/multicastRouting")]
+	Task<DeviceLiveToolsMulticastRoutingCreateResponse> CreateDeviceLiveToolsMulticastRoutingAsync(
+		string serial,
+		[Body] DeviceLiveToolsMulticastRoutingCreateRequest request,
 		CancellationToken cancellationToken = default
 	);
 }
