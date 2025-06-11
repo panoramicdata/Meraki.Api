81e1c5770d00a0bb0c76497e432aa97d293978f3
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 10:13:40 +0100

    Added generateAdministeredIdentitiesMeApiKeys method and models

--- Meraki.Api/Interfaces/General/Administered/IAdministeredIdentitiesMeApiKeys.cs
+++ Meraki.Api/Interfaces/General/Administered/IAdministeredIdentitiesMeApiKeys.cs
@@ -15,4 +15,18 @@ public interface IAdministeredIdentitiesMeApiKeys
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Generates an API key for an identity. For users who have access to more than one organization, the change will take up to five minutes to propagate. If one of the organizations is currently under maintenance, the change may not propagate fully until after the maintenance has been completed.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("generateAdministeredIdentitiesMeApiKeys")]
+	[Post("/administered/identities/me/api/keys/generate")]
+	Task<AdministeredIdentitiesMeGeneratedApiKey> GenerateAdministeredIdentitiesMeApiKeysAsync(
+		string organizationId,
+		CancellationToken cancellationToken = default
+	);
 }
