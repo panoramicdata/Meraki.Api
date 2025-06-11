7b3252c29a0af1c2ed97ce92fdba65f18d4df596
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 16:23:28 +0100

    Added createDeviceLiveToolsRoutingTable method and models

--- Meraki.Api/Interfaces/General/LiveTools/ILiveToolsRoutingTable.cs
+++ Meraki.Api/Interfaces/General/LiveTools/ILiveToolsRoutingTable.cs
@@ -15,4 +15,19 @@ public interface ILiveToolsRoutingTable
 		string serial,
 		string id,
 		CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Enqueue a job to perform a routing table request for the device. Only native Catalyst switches are supported. This endpoint has a sustained rate limit of one request every five seconds per device, with an allowed burst of five requests.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="serial"></param>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("createDeviceLiveToolsRoutingTable")]
+	[Post("/devices/{serial}/liveTools/routingTable")]
+	Task<DeviceLiveToolsRoutingTableCreateResponse> CreateDeviceLiveToolsRoutingTableAsync(
+		string serial,
+		[Body] DeviceLiveToolsRoutingTableCreateRequest request,
+		CancellationToken cancellationToken = default);
 }
