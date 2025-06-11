1ec838e61e91480f8a935f869d72273931f5206d
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 10:14:54 +0100

    Added revokeAdministeredIdentitiesMeApiKeys method

--- Meraki.Api/Interfaces/General/Administered/IAdministeredIdentitiesMeApiKeys.cs
+++ Meraki.Api/Interfaces/General/Administered/IAdministeredIdentitiesMeApiKeys.cs
@@ -29,4 +29,20 @@ public interface IAdministeredIdentitiesMeApiKeys
 		string organizationId,
 		CancellationToken cancellationToken = default
 	);
+
+	/// <summary>
+	/// Revokes an identity's API key, using the last four characters of the key. For users who have access to more than one organization, the change will take up to five minutes to propagate. If one of the organizations is currently under maintenance, the change may not propagate fully until after the maintenance has been completed.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId"></param>
+	/// <param name="suffix"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	[ApiOperationId("revokeAdministeredIdentitiesMeApiKeys")]
+	[Post("/administered/identities/me/api/keys/{suffix}/revoke")]
+	Task RevokeAdministeredIdentitiesMeApiKeysAsync(
+		string organizationId,
+		string suffix,
+		CancellationToken cancellationToken = default
+	);
 }
