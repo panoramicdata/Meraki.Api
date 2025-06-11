00220c9c0ba5a98fdf1b27f1ddfc46bf5ab44a7b
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-06-11 09:16:57 +0100

    Added getOrganizationWirelessDevicesRadsecCertificatesAuthorities method and models

---
+++ Meraki.Api/Data/OrganizationWirelessDevicesRadsecCertificatesAuthorities.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Organization Wireless Devices Radsec Certificates Authorities
+/// </summary>
+[DataContract]
+public class OrganizationWirelessDevicesRadsecCertificatesAuthorities
+{
+	/// <summary>
+	/// Metadata about the Certificate Authorities
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "meta")]
+	public OrganizationWirelessDevicesRadsecCertificatesAuthoritiesMetadata Meta { get; set; } = new();
+
+	/// <summary>
+	/// List of Certificate Authorities
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "items")]
+	public List<OrganizationWirelessDevicesRadsecCertificatesAuthoritiesItem> Items { get; set; } = [];
+}
--- Meraki.Api/Sections/General/Organizations/OrganizationsWirelessDevicesSection.cs
+++ Meraki.Api/Sections/General/Organizations/OrganizationsWirelessDevicesSection.cs
@@ -9,4 +9,6 @@ public class OrganizationsWirelessDevicesSection
 	public IOrganizationsWirelessDevicesPacketLoss PacketLoss { get; internal set; } = null!;
 
 	public IOrganizationsWirelessRadio Radio { get; internal set; } = null!;
+
+	public OrganizationsWirelessRadsecSection Radsec { get; internal set; } = new();
 }
