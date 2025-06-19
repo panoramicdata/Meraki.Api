4a07b6f47822d314f0f7e04974ac1df91ec78f86
Peter Acs <peter.acs@panoramicdata.com>
2025-06-19 16:25:08 +0200

    Added missing Organization Wireless Devices Packet Loss By Device endpoint

---
+++ Meraki.Api/Data/WirelessDevicePacketLoss.cs
@@ -0,0 +1,101 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Represents packet loss statistics for a wireless device in an organization.
+/// </summary>
+[DataContract]
+public class WirelessDevicePacketLoss
+{
+	/// <summary>
+	/// Downstream packet loss statistics.
+	/// </summary>
+	[DataMember(Name = "downstream")]
+	public PacketLossStats? Downstream { get; set; }
+
+	/// <summary>
+	/// Upstream packet loss statistics.
+	/// </summary>
+	[DataMember(Name = "upstream")]
+	public PacketLossStats? Upstream { get; set; }
+
+	/// <summary>
+	/// Network information for the device.
+	/// </summary>
+	[DataMember(Name = "network")]
+	public NetworkInfo? Network { get; set; }
+
+	/// <summary>
+	/// Device information.
+	/// </summary>
+	[DataMember(Name = "device")]
+	public DeviceInfo? Device { get; set; }
+}
+
+/// <summary>
+/// Represents packet loss statistics for a direction (upstream or downstream).
+/// </summary>
+[DataContract]
+public class PacketLossStats
+{
+	/// <summary>
+	/// Total number of packets.
+	/// </summary>
+	[DataMember(Name = "total")]
+	public int Total { get; set; }
+
+	/// <summary>
+	/// Number of lost packets.
+	/// </summary>
+	[DataMember(Name = "lost")]
+	public int Lost { get; set; }
+
+	/// <summary>
+	/// Percentage of lost packets.
+	/// </summary>
+	[DataMember(Name = "lossPercentage")]
+	public double LossPercentage { get; set; }
+}
+
+/// <summary>
+/// Represents network information.
+/// </summary>
+[DataContract]
+public class NetworkInfo
+{
+	/// <summary>
+	/// The network ID.
+	/// </summary>
+	[DataMember(Name = "id")]
+	public string Id { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The network name.
+	/// </summary>
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// Represents device information.
+/// </summary>
+[DataContract]
+public class DeviceInfo
+{
+	/// <summary>
+	/// The device name.
+	/// </summary>
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The device serial number.
+	/// </summary>
+	[DataMember(Name = "serial")]
+	public string Serial { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The device MAC address.
+	/// </summary>
+	[DataMember(Name = "mac")]
+	public string Mac { get; set; } = string.Empty;
+}
---
+++ Meraki.Api/Interfaces/Products/Wireless/IWirelessDevicePacketLoss.cs
@@ -0,0 +1,26 @@
+﻿namespace Meraki.Api.Interfaces.Products.Wireless;
+
+/// <summary>
+/// Provides functionality to retrieve packet loss information for wireless devices within an organization.
+/// </summary>
+/// <remarks>This interface defines a method for listing the most recent packet loss data for wireless devices.
+/// The data is paginated, and the caller can specify the number of entries per page.</remarks>
+public interface IWirelessDevicePacketLoss
+{
+	/// <summary>
+	/// List the most recent packet loss information for wireless devices.
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">The organization ID</param>
+	/// <param name="perPage">The number of entries per page returned. Acceptable range is 3 - 1000. Default is 100.</param>
+	/// <param name="startingAfter">A token used by the server to indicate the start of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.</param>
+	/// <param name="endingBefore">A token used by the server to indicate the end of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	[Get("/organizations/{organizationId}/wireless/devices/packetLoss/byDevice")]
+	Task<List<WirelessDevicePacketLoss>> GetOrganizationWirelessDevicesPacketLossAsync(
+		string organizationId,
+		int? perPage = 100,
+		string? startingAfter = null,
+		string? endingBefore = null,
+		CancellationToken cancellationToken = default);
+}
