a1d7441c5836b06650b614d4554677331d081c6d
Daniel Abbatt <daniel.abbatt@panoramicdata.com>
2025-07-03 10:29:44 +0100

    Update VLAN ID properties to be nullable
    Changed `DataVlanId` and `VoiceVlanId` in the `CriticalAuth` class from non-nullable integers to nullable integers. This modification allows these properties to hold null values, accommodating scenarios where VLAN IDs may not be applicable, such as when RADIUS authentication fails with Multi-Auth host mode.

--- Meraki.Api/Data/CriticalAuth.cs
+++ Meraki.Api/Data/CriticalAuth.cs
@@ -11,14 +11,14 @@ public class CriticalAuth
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "dataVlanId")]
-	public int DataVlanId { get; set; }
+	public int? DataVlanId { get; set; }
 
 	/// <summary>
 	/// VLAN that clients who use voice will be placed on when RADIUS authentication fails. Will be null if hostMode is Multi-Auth
 	/// </summary>
 	[ApiAccess(ApiAccess.ReadWrite)]
 	[DataMember(Name = "voiceVlanId")]
-	public int VoiceVlanId { get; set; }
+	public int? VoiceVlanId { get; set; }
 
 	/// <summary>
 	/// Enable to suspend port bounce when RADIUS servers are unreachable
