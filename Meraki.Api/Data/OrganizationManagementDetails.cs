79e38899e391abd830c245f2e0a34f8e355423c0
David Bond <david.bond@panoramicdata.com>
2025-07-21 16:46:54 +0100

    Added Organization Management Details

---
+++ Meraki.Api/Data/OrganizationManagementDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// Represents the management details of an organization, including its name and associated value.
+/// </summary>
+[DataContract]
+public class OrganizationManagementDetails
+{
+	/// <summary>
+	/// The name of the management detail.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The value of the management detail.
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "value")]
+	public string Value { get; set; } = string.Empty;
+}
