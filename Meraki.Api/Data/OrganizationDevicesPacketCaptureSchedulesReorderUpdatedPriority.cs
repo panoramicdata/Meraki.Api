9eba28a9d130b3d66480049fbe9d615cca997f84
Mathew Jones <mathew.jones@panoramicdata.com>
2025-08-30 08:37:23 +0100

    Deprecated property removed

--- Meraki.Api/Data/OrganizationDevicesPacketCaptureSchedulesReorderUpdatedPriority.cs
+++ Meraki.Api/Data/OrganizationDevicesPacketCaptureSchedulesReorderUpdatedPriority.cs
@@ -16,7 +16,15 @@ public class OrganizationDevicesPacketCaptureSchedulesReorderUpdatedPriority
 	/// <summary>
 	/// Id of the scheduled packet capture
 	/// </summary>
+	[Obsolete("This property removed in API v1.59")]
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "pcapScheduleConfigurationId")]
 	public string PcapScheduleConfigurationId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Id of the scheduled packet capture
+	/// </summary>
+	[ApiAccess(ApiAccess.Read)]
+	[DataMember(Name = "scheduleId")]
+	public string ScheduleId { get; set; } = string.Empty;
 }
