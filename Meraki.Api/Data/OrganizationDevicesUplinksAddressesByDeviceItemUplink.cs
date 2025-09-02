eb2b3b5978c78f81903692b96e723344a8ed7cad
Mathew Jones <mathew.jones@panoramicdata.com>
2025-09-02 19:25:14 +0100

    Addresses relabelled

--- Meraki.Api/Data/OrganizationDevicesUplinksAddressesByDeviceItemUplink.cs
+++ Meraki.Api/Data/OrganizationDevicesUplinksAddressesByDeviceItemUplink.cs
@@ -14,7 +14,8 @@ public class OrganizationDevicesUplinksAddressesByDeviceItemUplink
 	public UplinkInterface Interface { get; set; }
 
 	/// <summary>
-	/// List of device uplink addresses information.
+	/// Available addresses for the interface. Each interface may have one ipv4 and one ipv6 address.
+
 	/// </summary>
 	[ApiAccess(ApiAccess.Read)]
 	[DataMember(Name = "addresses")]
