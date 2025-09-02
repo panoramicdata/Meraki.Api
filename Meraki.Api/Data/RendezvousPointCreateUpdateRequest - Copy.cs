f9c8a7f9293fcc0369f34e0c7e479947cac32a6b
Mathew Jones <mathew.jones@panoramicdata.com>
2025-09-02 17:20:44 +0100

    RendezvousPoinr Vrf added

---
+++ Meraki.Api/Data/RendezvousPointCreateUpdateRequest - Copy.cs
@@ -0,0 +1,16 @@
+﻿namespace Meraki.Api.Data;
+
+/// <summary>
+/// The VRF with PIM enabled L3 interface
+/// </summary>
+[DataContract]
+public class RendezvousPointVrf
+{
+	/// <summary>
+	/// The name of the VRF
+	/// </summary>
+	[ApiAccess(ApiAccess.ReadWrite)]
+	[DataMember(Name = "name")]
+	public string Name { get; set; } = string.Empty;
+
+}
