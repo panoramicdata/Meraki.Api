730659d11f86b220bad1c4fab47319998b50d410
Mathew Jones <mathew.jones@panoramicdata.com>
2025-09-29 16:12:50 +0100

    Get Space Integration Status

--- Meraki.Api/Interfaces/General/Organizations/IOrganizationsSpacesIntegration.cs
+++ Meraki.Api/Interfaces/General/Organizations/IOrganizationsSpacesIntegration.cs
@@ -3,13 +3,26 @@
 public interface IOrganizationsSpacesIntegration
 {
 	/// <summary>
+	/// Get the status of the Spaces integration in Meraki
+	/// </summary>
+	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
+	/// <param name="organizationId">Organization ID</param>
+	/// <param name="cancellationToken">The cancellation token</param>
+	[ApiOperationId("getOrganizationSpacesIntegrateStatus")]
+	[Get("/organizations/{organizationId}/spaces/integrate/status")]
+	Task<OrganizationSpacesIntegrateStatus> GetOrganizationSpacesIntegrateStatusAsync(
+		string organizationId,
+		CancellationToken cancellationToken = default);
+
+	/// <summary>
 	/// Remove the Spaces integration from Meraki
 	/// </summary>
 	/// <exception cref="ApiException">Thrown when fails to make API call</exception>
-	/// <param name="organizationId"></param>
-	/// <param name="cancellationToken"></param>
-	/// <returns></returns>
+	/// <param name="organizationId">Organization ID</param>
+	/// <param name="cancellationToken">The cancellation token</param>
 	[ApiOperationId("removeOrganizationSpacesIntegration")]
 	[Post("/organizations/{organizationId}/spaces/integration/remove")]
-	Task<OrganizationSpacesIntegrationActionResponse> RemoveOrganizationSpacesIntegrationAsync(string organizationId, CancellationToken cancellationToken = default);
+	Task<OrganizationSpacesIntegrationActionResponse> RemoveOrganizationSpacesIntegrationAsync(
+		string organizationId, 
+		CancellationToken cancellationToken = default);
 }
