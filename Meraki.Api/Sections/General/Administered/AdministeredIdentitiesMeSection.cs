12ecbea8fd7df677c42d2b8789e78c0ab9d19d2e
David Bond <david.bond@panoramicdata.com>
2025-11-12 01:08:26 +0000

    XML comments added throughout

--- Meraki.Api/Sections/General/Administered/AdministeredIdentitiesMeSection.cs
+++ Meraki.Api/Sections/General/Administered/AdministeredIdentitiesMeSection.cs
@@ -2,14 +2,19 @@ using Meraki.Api.Interfaces.General.Administered;
 
 namespace Meraki.Api.Sections.General.Administered;
 
+/// <summary>
+/// Represents the section of the API that provides operations for the current administered identity.
+/// </summary>
 public partial class AdministeredIdentitiesMeSection
 {
+	/// <summary>
+	/// Gets or sets the API for operations related to the current administered identity.
+	/// </summary>
 	[RefitPromoteCalls]
 	internal IAdministeredIdentitiesMe Me { get; set; } = null!;
 
 	/// <summary>
 	/// Gets the api
 	/// </summary>
-
 	public AdministeredIdentitiesMeApiSection Api { get; internal set; } = new();
 }
--- Meraki.Api/Sections/Products/Appliance/ApplianceContentFilteringSection.cs
+++ Meraki.Api/Sections/Products/Appliance/ApplianceContentFilteringSection.cs
@@ -1,5 +1,11 @@
 namespace Meraki.Api.Sections.Products.Appliance;
 
+/// <summary>
+/// Provides access to content filtering operations and categories for an MX network.
+/// </summary>
+/// <remarks>This section exposes APIs for managing and retrieving information related to content filtering on
+/// Meraki MX networks. Use the <see cref="Categories"/> property to list available filtering categories. Additional
+/// content filtering operations may be available through other members of this class.</remarks>
 public partial class ApplianceContentFilteringSection
 {
 	[RefitPromoteCalls]
@@ -7,6 +13,5 @@ public partial class ApplianceContentFilteringSection
 	/// <summary>
 	/// List all available content filtering categories for an MX network
 	/// </summary>
-
 	public IApplianceContentFilteringCategories Categories { get; internal set; } = null!;
 }
